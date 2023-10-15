d591a9720f9b279a06da3bc874c4e17acf1f7b9d
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-10-15 13:25:52 -0400

    fixes

---
+++ apps/nextjs/src/components/developers/sarinali.tsx
@@ -0,0 +1,33 @@
+// sarina component
+import { api } from "~/utils/api";
+
+const SarinaLi = () => {
+  const sarinaliInfo = api.developer.sarinali_info.useQuery().data;
+  const mutation = api.developer.sarinali_upvote.useMutation();
+  return (
+    <div>
+      <h1>sarinaliInfo from API:</h1>
+      {sarinaliInfo ? (
+        <div>
+          <p>Name: {sarinaliInfo.name}</p>
+          <p>Year: {sarinaliInfo.year}</p>
+          <p>Introduction: {sarinaliInfo.introduction}</p>
+          <p>Favorite Food: {sarinaliInfo.fav_food}</p>
+          <p>Favorite Song: {sarinaliInfo.fav_song}</p>
+          <p>Upvotes: {sarinaliInfo.upvotes}</p>
+        </div>
+      ) : (
+        <p>Loading sarinaliInfo...</p>
+      )}
+      <button
+        onClick={() => {
+          mutation.mutate();
+        }}
+      >
+        Increase Upvote
+      </button>
+    </div>
+  );
+};
+
+export default SarinaLi;
