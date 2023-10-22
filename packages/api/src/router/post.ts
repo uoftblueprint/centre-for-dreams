54aba4ed657f175d1c712a33864698e2ce668fcb
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-10-22 12:58:11 -0400

    updated formatting

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
@@ -7,30 +8,42 @@ export const postRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     });
   }),
-  createPost: protectedProcedure.input(z.object({ title: z.string(), contents: z.string()})).mutation(async({ctx, input}) => {
-    await ctx.db.post.create({
-      data: {
-        title: input.title,
-        contents: input.contents,
-      }
-    })
-  }),
-  updatePostByID: protectedProcedure.input(z.object({ id: z.number(), title: z.string().optional(), contents: z.string().optional()})).mutation(async({ctx, input}) => {
-    await ctx.db.post.update({
-      where: {
-        id: input.id
-      }, 
-      data: {
-        title: input.title,
-        contents: input.contents
-      },
-    })
-  }),
-  deletePostByID: protectedProcedure.input(z.object({ id: z.number()})).mutation(async({ctx, input}) => {
-    await ctx.db.post.delete({
-      where: {
-        id: input.id
-      }
-    })
-  }),
+  createPost: protectedProcedure
+    .input(z.object({ title: z.string(), contents: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.post.create({
+        data: {
+          title: input.title,
+          contents: input.contents,
+        },
+      });
+    }),
+  updatePostByID: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        title: z.string().optional(),
+        contents: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.post.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          title: input.title,
+          contents: input.contents,
+        },
+      });
+    }),
+  deletePostByID: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.post.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
 });
