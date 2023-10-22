6106030eb76cf29798fb9e5d3affd7309053fa52
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-10-22 12:41:24 -0400

    made create post endpoint

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
@@ -6,4 +7,13 @@ export const postRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     });
   }),
+  createPost: protectedProcedure.input(z.object({ title: z.string(), contents: z.string()})).mutation(async({ctx, input}) => {
+    await ctx.db.post.create({
+      data: {
+        title: input.title,
+        contents: input.contents,
+      }
+    })
+  }),
+  
 });
