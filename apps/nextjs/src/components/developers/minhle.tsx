05c2bcb86be0edac38fb9377859bf975ef9f39a7
Minh Le <ngocminhle318@gmail.com>
2023-10-22 02:07:55 -0400

    idek anymore

--- apps/nextjs/src/components/developers/minhle.tsx
+++ apps/nextjs/src/components/developers/minhle.tsx
@@ -6,32 +6,30 @@ const MinhLe = () => {
   const minh_upvote = api.developer.minhle_upvote.useMutation();
 
   return (
-    <div className="border p-4 rounded shadow max-w-sm">
-        <h1>
-            {minh_info?.name}
-        </h1>
-        <p>
-            <b>Year:</b> {minh_info?.year}
-        </p>
-        <p>
-            <b>Introduction:</b> {minh_info?.introduction}
-        </p>
-        <p>
-            <b>Favourite Food:</b> {minh_info?.fav_food}
-        </p>
-        <p>
-            <b>Favourite Song:</b> {minh_info?.fav_song}
-        </p>
-        <p>
-            <b>Upvotes:</b> {minh_info?.upvotes}
-        </p>
+    <div className="max-w-sm rounded border p-4 shadow">
+      <h1>{minh_info?.name}</h1>
+      <p>
+        <b>Year:</b> {minh_info?.year}
+      </p>
+      <p>
+        <b>Introduction:</b> {minh_info?.introduction}
+      </p>
+      <p>
+        <b>Favourite Food:</b> {minh_info?.fav_food}
+      </p>
+      <p>
+        <b>Favourite Song:</b> {minh_info?.fav_song}
+      </p>
+      <p>
+        <b>Upvotes:</b> {minh_info?.upvotes}
+      </p>
 
-        <button 
-            onClick={() => minh_upvote.mutate()}  
-            className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-
-            Upvote!
-        </button>
+      <button
+        onClick={() => minh_upvote.mutate()}
+        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+      >
+        Upvote!
+      </button>
     </div>
   );
 };
