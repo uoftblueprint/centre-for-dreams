35bd2534162933e4734ca155f05afa7b361d4e27
RyanL123 <ryan.lichangrui@gmail.com>
2023-11-04 19:33:23 -0400

    fix eslint errors

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { SignIn, SignInButton, useAuth, UserButton } from "@clerk/nextjs";
+import { SignInButton, useAuth, UserButton } from "@clerk/nextjs";
 
 import HelenaGlowacki from "~/components/developers/helenaglowacki";
 import HelenZhao from "~/components/developers/helenzhao";
