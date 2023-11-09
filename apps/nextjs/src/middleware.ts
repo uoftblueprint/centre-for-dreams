61d566cbf1065e2bc5e751cfa767a83c6607c18e
RyanL123 <ryan.lichangrui@gmail.com>
2023-11-08 19:23:24 -0500

    clearer comments in middlware.ts

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -2,7 +2,9 @@ import { authMiddleware } from "@clerk/nextjs";
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/(api|trpc)(.*)"], // Do not protect '/' for now since that's where we land
+  // "/" is not protected since that's where we land
+  // api routes are not protected since authentication is done via protected routes in trpc.ts
+  publicRoutes: ["/", "/(api|trpc)(.*)"],
 });
 
 // See https://nextjs.org/docs/pages/building-your-application/routing/middleware#matcher for what config does
