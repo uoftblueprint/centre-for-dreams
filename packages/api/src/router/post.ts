d10b7b5c3e6cf34cc89a92193c8b432bcbfd4439
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-11-08 23:00:09 -0500

    add: separate routes for discussions and announcements

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,6 +1,70 @@
+import type { SignedInAuthObject } from "@clerk/clerk-sdk-node";
+import type { PrismaClient } from "@prisma/client";
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { adminProcedure, createTRPCRouter, protectedProcedure } from "../trpc";
+
+const basePostInput = {
+  title: z.string().trim().min(1).max(300),
+  contents: z.string().min(1),
+};
+
+const createPostInput = z.object({
+  ...basePostInput,
+});
+
+const updatePostInput = z
+  .object({
+    id: z.number().nonnegative(),
+    ...basePostInput,
+  })
+  .partial();
+
+const deletePostInput = z.object({
+  id: z.number().nonnegative(),
+});
+
+const createPost = async (
+  ctx: { db: PrismaClient; auth: SignedInAuthObject },
+  input: z.infer<typeof createPostInput>,
+  postType: "Announcement" | "Discussion",
+) => {
+  await ctx.db.post.create({
+    data: {
+      ...input,
+      postType,
+    },
+  });
+};
+
+const updatePost = async (
+  ctx: { db: PrismaClient; auth: SignedInAuthObject },
+  input: z.infer<typeof updatePostInput>,
+  postType: "Announcement" | "Discussion",
+) => {
+  await ctx.db.post.update({
+    where: {
+      id: input.id,
+      postType,
+    },
+    data: {
+      ...input,
+    },
+  });
+};
+
+const deletePost = async (
+  ctx: { db: PrismaClient; auth: SignedInAuthObject },
+  input: z.infer<typeof updatePostInput>,
+  postType: "Announcement" | "Discussion",
+) => {
+  await ctx.db.post.delete({
+    where: {
+      id: input.id,
+      postType,
+    },
+  });
+};
 
 export const postRouter = createTRPCRouter({
   getPosts: protectedProcedure.query(async ({ ctx }) => {
@@ -9,50 +73,33 @@ export const postRouter = createTRPCRouter({
     });
   }),
   createPost: protectedProcedure
-    .input(
-      z.object({
-        title: z.string().trim().min(1).max(300),
-        contents: z.string().min(1),
-        postType: z.enum(["Announcement", "Discussion"]),
-      }),
-    )
+    .input(createPostInput)
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.post.create({
-        data: {
-          title: input.title,
-          contents: input.contents,
-          postType: input.postType,
-        },
-      });
+      await createPost(ctx, input, "Discussion");
+    }),
+  createAnnouncement: adminProcedure
+    .input(createPostInput)
+    .mutation(async ({ ctx, input }) => {
+      await createPost(ctx, input, "Announcement");
     }),
   updatePostByID: protectedProcedure
-    .input(
-      z.object({
-        id: z.number().nonnegative(),
-        title: z.string().optional(),
-        contents: z.string().optional(),
-        postType: z.enum(["Announcement", "Discussion"]),
-      }),
-    )
+    .input(updatePostInput)
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.post.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          title: input.title,
-          contents: input.contents,
-          postType: input.postType,
-        },
-      });
+      await updatePost(ctx, input, "Discussion");
+    }),
+  updateAnnouncementByID: adminProcedure
+    .input(updatePostInput)
+    .mutation(async ({ ctx, input }) => {
+      await updatePost(ctx, input, "Announcement");
     }),
   deletePostByID: protectedProcedure
-    .input(z.object({ id: z.number().nonnegative() }))
+    .input(deletePostInput)
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.post.delete({
-        where: {
-          id: input.id,
-        },
-      });
+      await deletePost(ctx, input, "Discussion");
+    }),
+  deleteAnnouncementByID: adminProcedure
+    .input(deletePostInput)
+    .mutation(async ({ ctx, input }) => {
+      await deletePost(ctx, input, "Announcement");
     }),
 });
