db30b8212c22994f87894fda1f7186f1f85a242a
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-11-09 01:35:31 -0500

    change: push token model id to token

--- packages/db/prisma/migrations/20231109063448_add_push_token_model/migration.sql
+++ packages/db/prisma/migrations/20231109063448_add_push_token_model/migration.sql
@@ -2,11 +2,10 @@
 CREATE TABLE "PushToken" (
     "token" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
-    "isValid" BOOLEAN NOT NULL DEFAULT true
-);
+    "isValid" BOOLEAN NOT NULL DEFAULT true,
 
--- CreateIndex
-CREATE UNIQUE INDEX "PushToken_token_userId_key" ON "PushToken"("token", "userId");
+    CONSTRAINT "PushToken_pkey" PRIMARY KEY ("token")
+);
 
 -- AddForeignKey
 ALTER TABLE "PushToken" ADD CONSTRAINT "PushToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
