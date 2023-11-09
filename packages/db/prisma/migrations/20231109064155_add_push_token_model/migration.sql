e3bbc80425597f872865f8a030af21683153306a
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-11-09 01:42:27 -0500

    change: push token field from @id to @unique

--- packages/db/prisma/migrations/20231109064155_add_push_token_model/migration.sql
+++ packages/db/prisma/migrations/20231109064155_add_push_token_model/migration.sql
@@ -2,10 +2,11 @@
 CREATE TABLE "PushToken" (
     "token" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
-    "isValid" BOOLEAN NOT NULL DEFAULT true,
-
-    CONSTRAINT "PushToken_pkey" PRIMARY KEY ("token")
+    "isValid" BOOLEAN NOT NULL DEFAULT true
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PushToken_token_key" ON "PushToken"("token");
 
 -- AddForeignKey
 ALTER TABLE "PushToken" ADD CONSTRAINT "PushToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
