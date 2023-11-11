5764b0df60125d54351ae36b1e464fb590a24325
helenaglow <ikcawolgh@outlook.com>
2023-11-11 11:50:45 -0600

    Fix formatting

--- apps/nextjs/src/components/absence.tsx
+++ apps/nextjs/src/components/absence.tsx
@@ -1,13 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface AbsenceProps {
-    data: {
-        userId: number;
-        absenceDate: Date;
-    };
-  }
-const Absence: React.FC<AbsenceProps> = ({data : {userId, absenceDate}}) => {
-
+  data: {
+    userId: number;
+    absenceDate: Date;
+  };
+}
+const Absence: React.FC<AbsenceProps> = ({ data: { userId, absenceDate } }) => {
   return (
     <div className="m-4 block max-w-sm rounded-lg border border-gray-200 bg-white p-4 shadow">
       <h2 className="mb-4 text-xl font-bold ">Absence</h2>
--- apps/nextjs/src/pages/absences.tsx
+++ apps/nextjs/src/pages/absences.tsx
@@ -1,43 +1,40 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
+
 import { api } from "~/utils/api";
 import Absence from "../components/absence";
 
 export default function Absences() {
-    const { data: allAbsences } = api.absence.getAllAbsences.useQuery();
-    const [startDate, setStartDate] = useState(new Date());
+  const { data: allAbsences } = api.absence.getAllAbsences.useQuery();
+  const [startDate, setStartDate] = useState(new Date());
 
-    const areDatesEqual = (date1, date2) => {
-        return (
-        date1.getFullYear() === date2.getFullYear() &&
-        date1.getMonth() === date2.getMonth() &&
-        date1.getDate() === date2.getDate()
-        );
-    };
+  const areDatesEqual = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
 
-    const filteredAbsences = allAbsences
-        ?.filter((absence) => !startDate || areDatesEqual(absence.absenceDate, startDate))
-        .map((data) => <Absence key={data.id} data={data} />);
-
-  
+  const filteredAbsences = allAbsences
+    ?.filter(
+      (absence) => !startDate || areDatesEqual(absence.absenceDate, startDate),
+    )
+    .map((data) => <Absence key={data.id} data={data} />);
 
   return (
     <>
-        <h1 className="m-4 text-2xl font-extrabold ">Absences</h1>
-        <div className="m-4">
-          <span>Pick a date: </span>
-          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-        </div>
-        {filteredAbsences}
+      <h1 className="m-4 text-2xl font-extrabold ">Absences</h1>
+      <div className="m-4">
+        <span>Pick a date: </span>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+      </div>
+      {filteredAbsences}
     </>
-    
   );
 }
-
-
-
-
-
-
-
