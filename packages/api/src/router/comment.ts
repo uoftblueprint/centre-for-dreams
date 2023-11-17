e1b710a2510481c084343d1c69c3908a76061160
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-11-17 16:32:45 -0500

    Deleted comments endpoint

--- packages/api/src/router/comment.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCRouter, protectedProcedure } from "../trpc";
-
-export const commentRouter = createTRPCRouter({
-  getComments: protectedProcedure.query(async ({ ctx }) => {
-    return await ctx.db.comment.findMany({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
-});
