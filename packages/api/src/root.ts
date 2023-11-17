5fc72dcfd6d37372b8378dfc2c3e41d22fd27e06
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-11-17 16:33:59 -0500

    removed comment router from root.ts

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,4 +1,3 @@
-import { commentRouter } from "./router/comment";
 import { developerRouter } from "./router/developer";
 import { postRouter } from "./router/post";
 import { createTRPCRouter } from "./trpc";
@@ -6,7 +5,6 @@ import { createTRPCRouter } from "./trpc";
 export const appRouter = createTRPCRouter({
   developer: developerRouter,
   post: postRouter,
-  comment: commentRouter,
 });
 
 // export type definition of API
