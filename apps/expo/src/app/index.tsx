26d440154d3edb13eeab1d791c3fc0acc61b1a21
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-17 21:45:49 -0500

    finished announcement component

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
-
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
-import Announcement from "~/components/announcement";
+import { Text, View } from "react-native";
 
 Notifications.setNotificationHandler({
   handleNotification: () =>
@@ -15,6 +13,9 @@ Notifications.setNotificationHandler({
       shouldSetBadge: false,
     }),
 });
+
+// const imageUrl = 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857';
+// const imageSourceProp: ImageSourcePropType = { uri: imageUrl };
 
 const Index = () => {
   const [, setExpoPushToken] = useState<Notifications.ExpoPushToken | null>(
@@ -30,10 +31,10 @@ const Index = () => {
     <SafeAreaView className="">
       {/* Changes page title visible on the header */}
       <Stack.Screen options={{ title: "Home Page" }} />
-      {/* <View className="h-full w-full p-4">
+      <View className="h-full w-full p-4">
         <Text> Index</Text>
-      </View> */}
-      <Announcement title="hell0" author='hi' date="Nov 15" summary="helooo" ></Announcement>
+      </View>
+      {/* <Announcement title="hell0" author='hi' date="Nov 15" summary="helooo" profile_picture={imageSourceProp} post_media={imageSourceProp}></Announcement> */}
     </SafeAreaView>
   );
 };
--- apps/expo/src/components/announcement.tsx
+++ apps/expo/src/components/announcement.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Text, View, Image } from "react-native";
+import type { ImageSourcePropType } from 'react-native';
+import { Entypo } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 interface Props {
     title: string;
     author: string;
     date: string;
     summary: string;
+    profile_picture: ImageSourcePropType;
+    post_media: ImageSourcePropType;
   }
-  
 
-export default function Announcement({ title, author, date, summary }: Props) {
+export default function Announcement({ title, author, date, summary, profile_picture, post_media }: Props) {
     return (
-        <View className='bg-grey-800 p-4 rounded-lg h-25 mx-auto w-11/12'>
+        <View className='bg-white p-4 rounded-lg h-25 mx-auto w-11/12'>
             <Text className='font-bold text-xl'>{title}</Text>
             <View className='flex-row items-center mt-2'>
               <Image 
-                  source={{ uri: 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857' }}
+                  source={profile_picture}
                   className='w-8 h-8 rounded-full'
                   resizeMode="cover"
               />
@@ -24,16 +28,18 @@ export default function Announcement({ title, author, date, summary }: Props) {
             </View>
             <View className='mt-2'>
             <Image 
-                  source={{ uri: 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857' }}
+                  source={post_media}
                   className='w-fit h-60'
               />
             </View>
             <View>
               <Text className='mt-2 text-lg'>{summary}</Text>
             </View>
-            <View className='flex-row justify-end items-center'>
-                <Text className='mr-2'>Reply</Text>
-                <Text>Read</Text>
+            <View className='flex-row items-center justify-end'>
+                <Entypo name="reply" size={24} color="black"/>
+                <Text className='mr-2 text-lg ml-2'>Reply</Text>
+                <MaterialCommunityIcons name="read" size={24} color="black" />
+                <Text className='ml-2 text-lg'>Read</Text>
             </View>
         </View>
       );
