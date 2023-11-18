d857f948cef584de662eae9511257d47d5f45c05
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-17 22:29:41 -0500

    added items in comment

--- apps/expo/src/app/components/comment.tsx
+++ apps/expo/src/app/components/comment.tsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
 import type { ImageSourcePropType } from "react-native";
+import { AntDesign } from '@expo/vector-icons'; 
+
 
 interface Props {
   user: string;
-  // user_pic: ImageSourcePropType
+  user_pic: ImageSourcePropType
   time_elapsed: string;
   comment_body: string;
 }
 
 export default function Comment({
-  user, time_elapsed, comment_body
+  user, user_pic, time_elapsed, comment_body
 }: Props) {
   return (
     <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-4">
-      
+      <View className="flex-row items-center justify-start">
+      <Image
+          source={user_pic}
+          className="h-8 w-8 rounded-full"
+          resizeMode="cover"
+        />
+        <Text>{user}</Text>
+        <AntDesign name="clockcircle" size={24} color="black" />
+        <Text>{time_elapsed}</Text>
+      </View>
+      <Text>{comment_body}</Text>
     </View>
   );
 }
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -4,8 +4,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
 import Comment from "./components/comment"
+import type { ImageSourcePropType } from "react-native";
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
+const imageUrl = 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857';
+const imageSourceProp: ImageSourcePropType = { uri: imageUrl };
 
 Notifications.setNotificationHandler({
   handleNotification: () =>
@@ -33,7 +36,7 @@ const Index = () => {
       {/* <View className="h-full w-full p-4">
         <Text> Index </Text>
       </View> */}
-      <Comment user="Sarina" time_elapsed="10h" comment_body="thank you so much for merging my PR so quickly i love my PLs and they are awesome #ilovebakerandganesh"></Comment>
+      <Comment user="Sarina" user_pic={imageSourceProp} time_elapsed="10h" comment_body="thank you so much for merging my PR so quickly i love my PLs and they are awesome #ilovebakerandganesh"></Comment>
     </SafeAreaView>
   );
 };
