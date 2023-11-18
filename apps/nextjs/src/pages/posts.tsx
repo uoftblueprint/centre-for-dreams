8b66350ad76a9d99b1472a2e8719a6cc1ed08642
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-11-18 12:24:21 -0500

    Added type inference in posts.tsx

--- apps/nextjs/src/pages/posts.tsx
+++ apps/nextjs/src/pages/posts.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+import type { RouterOutputs } from "@cfd/api";
+
 import { api } from "~/utils/api";
 import Post from "../components/post";
 
+type GetPostOutput = RouterOutputs["post"]["getPosts"][number];
+
 function Posts() {
   const posts = api.post.getPosts.useQuery();
-  return <>{posts.data?.map((p) => <Post key={p.id} {...p} />)}</>;
+  return (
+    <>{posts.data?.map((p: GetPostOutput) => <Post key={p.id} {...p} />)}</>
+  );
 }
 
 export default Posts;
