0772a68a28fc9c1736f33caaa705acb06b2a4109
Helen <helenzhao42@gmail.com>
2023-11-18 11:23:57 -0500

    fix: User must be connected to a Participant

--- packages/db/prisma/migrations/20231118162145_add_participant_model_and_update_absence_model/migration.sql
+++ packages/db/prisma/migrations/20231118162145_add_participant_model_and_update_absence_model/migration.sql
@@ -3,7 +3,9 @@
 
   - You are about to drop the column `userId` on the `Absence` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[absenceDate,participantId]` on the table `Absence` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[participantId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `participantId` to the `Absence` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `participantId` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -16,23 +18,25 @@ DROP INDEX "Absence_absenceDate_userId_key";
 ALTER TABLE "Absence" DROP COLUMN "userId",
 ADD COLUMN     "participantId" INTEGER NOT NULL;
 
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "participantId" INTEGER NOT NULL;
+
 -- CreateTable
 CREATE TABLE "Participant" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Participant_userId_key" ON "Participant"("userId");
+CREATE UNIQUE INDEX "Absence_absenceDate_participantId_key" ON "Absence"("absenceDate", "participantId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Absence_absenceDate_participantId_key" ON "Absence"("absenceDate", "participantId");
+CREATE UNIQUE INDEX "User_participantId_key" ON "User"("participantId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Absence" ADD CONSTRAINT "Absence_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Participant" ADD CONSTRAINT "Participant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
