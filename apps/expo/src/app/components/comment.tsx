9f0184bbce643645a075afe58c3a5f952ee8a929
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-17 22:35:55 -0500

    styled comment component

--- apps/expo/src/app/components/comment.tsx
+++ apps/expo/src/app/components/comment.tsx
@@ -1,32 +1,34 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import type { ImageSourcePropType } from "react-native";
-import { AntDesign } from '@expo/vector-icons'; 
-
+import { AntDesign } from "@expo/vector-icons";
 
 interface Props {
   user: string;
-  user_pic: ImageSourcePropType
+  user_pic: ImageSourcePropType;
   time_elapsed: string;
   comment_body: string;
 }
 
 export default function Comment({
-  user, user_pic, time_elapsed, comment_body
+  user,
+  user_pic,
+  time_elapsed,
+  comment_body,
 }: Props) {
   return (
-    <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-4">
+    <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-6">
       <View className="flex-row items-center justify-start">
-      <Image
+        <Image
           source={user_pic}
           className="h-8 w-8 rounded-full"
           resizeMode="cover"
         />
-        <Text>{user}</Text>
-        <AntDesign name="clockcircle" size={24} color="black" />
-        <Text>{time_elapsed}</Text>
+        <Text className="ml-2 mr-2 text-base">{user}</Text>
+        <AntDesign name="clockcircle" size={20} color="black" />
+        <Text className="ml-2 text-base">{time_elapsed}</Text>
       </View>
-      <Text>{comment_body}</Text>
+      <Text className="ml-10 mt-2">{comment_body}</Text>
     </View>
   );
 }
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -3,12 +3,11 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
-import Comment from "./components/comment"
-import type { ImageSourcePropType } from "react-native";
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
-const imageUrl = 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857';
-const imageSourceProp: ImageSourcePropType = { uri: imageUrl };
+
+// const imageUrl = 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857';
+// const imageSourceProp: ImageSourcePropType = { uri: imageUrl };
 
 Notifications.setNotificationHandler({
   handleNotification: () =>
@@ -33,10 +32,10 @@ const Index = () => {
     <SafeAreaView className="">
       {/* Changes page title visible on the header */}
       <Stack.Screen options={{ title: "Home Page" }} />
-      {/* <View className="h-full w-full p-4">
+      <View className="h-full w-full p-4">
         <Text> Index </Text>
-      </View> */}
-      <Comment user="Sarina" user_pic={imageSourceProp} time_elapsed="10h" comment_body="thank you so much for merging my PR so quickly i love my PLs and they are awesome #ilovebakerandganesh"></Comment>
+      </View>
+      {/* <Comment user="Sarina" user_pic={imageSourceProp} time_elapsed="10h" comment_body="thank you so much for merging my PR so quickly i love my PLs and they are awesome #ilovebakerandganesh"></Comment> */}
     </SafeAreaView>
   );
 };
