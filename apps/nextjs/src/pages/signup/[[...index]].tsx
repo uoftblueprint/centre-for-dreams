f301a6d62667efcdb8fdc8e2968902e34733972c
RyanL123 <ryan.lichangrui@gmail.com>
2023-11-18 19:15:47 -0500

    use catch all routes

--- apps/nextjs/src/pages/signup/[[...index]].tsx
+++ apps/nextjs/src/pages/signup/[[...index]].tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <div className="flex h-screen items-center justify-center">
       <SignUp />
