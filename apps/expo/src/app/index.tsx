a5980680e8d7ed9d20625215368d6945ef7d952f
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-18 23:35:30 -0500

    deleted comments

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -6,9 +6,6 @@ import { Stack } from "expo-router";
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
 
-// const imageUrl = 'https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857';
-// const imageSourceProp: ImageSourcePropType = { uri: imageUrl };
-
 Notifications.setNotificationHandler({
   handleNotification: () =>
     Promise.resolve({
@@ -35,7 +32,6 @@ const Index = () => {
       <View className="h-full w-full p-4">
         <Text> Index </Text>
       </View>
-      {/* <Comment user="Sarina" user_pic={imageSourceProp} time_elapsed="10h" comment_body="thank you so much for merging my PR so quickly i love my PLs and they are awesome #ilovebakerandganesh"></Comment> */}
     </SafeAreaView>
   );
 };
