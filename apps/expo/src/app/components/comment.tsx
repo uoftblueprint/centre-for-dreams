4ad9cbb65cf1f841ab12f42de4b1529a6f5a0d8a
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-11-25 17:32:50 -0500

    added placeholder url

--- apps/expo/src/app/components/comment.tsx
+++ apps/expo/src/app/components/comment.tsx
@@ -1,32 +1,27 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
-import type { ImageSourcePropType } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 interface Props {
   user: string;
-  user_pic: ImageSourcePropType;
   time_elapsed: string;
   comment_body: string;
 }
 
-export default function Comment({
-  user,
-  user_pic,
-  time_elapsed,
-  comment_body,
-}: Props) {
+export default function Comment({ user, time_elapsed, comment_body }: Props) {
   return (
     <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-6">
       <View className="flex-row items-center justify-start">
         <Image
-          source={user_pic}
+          source={{
+            uri: "https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857",
+          }}
           className="h-8 w-8 rounded-full"
           resizeMode="cover"
         />
         <Text className="ml-2 mr-2 text-base">{user}</Text>
         <AntDesign name="clockcircle" size={20} color="black" />
-        <Text className="ml-2 text-base">{time_elapsed}</Text>
+        <Text className="ml-2 text-base">{time_elapsed + "h"}</Text>
       </View>
       <Text className="ml-10 mt-2">{comment_body}</Text>
     </View>
