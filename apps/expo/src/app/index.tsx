ceea4362eb831f617f0739f6f83f3ad6c0146e87
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2023-11-28 01:40:14 -0500

    add: poppins font to expo

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
 
@@ -19,18 +20,26 @@ const Index = () => {
   const [, setExpoPushToken] = useState<Notifications.ExpoPushToken | null>(
     null,
   );
+  const [fontsLoaded, fontError] = useFonts({
+    Poppins_400Regular,
+  });
 
   useEffect(() => {
     registerForPushNotificationsAsync()
       .then((token) => setExpoPushToken(token ?? null))
       .catch((err) => console.error(err));
   }, []);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <SafeAreaView className="">
       {/* Changes page title visible on the header */}
       <Stack.Screen options={{ title: "Home Page" }} />
       <View className="h-full w-full p-4">
-        <Text> Index </Text>
+        <Text style={{ fontFamily: 'Poppins_400Regular' }}> Index </Text>
       </View>
     </SafeAreaView>
   );
