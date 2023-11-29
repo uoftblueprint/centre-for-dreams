2cc3465159013f99400d85277eb48cd6b734fb72
RyanL123 <ryan.lichangrui@gmail.com>
2023-11-28 23:46:11 -0500

    use clerk metadata

--- apps/nextjs/src/pages/admindashboard.tsx
+++ apps/nextjs/src/pages/admindashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 
@@ -6,44 +6,26 @@ import { api } from "~/utils/api";
 
 export default function AdminDashboard() {
   const { user, isLoaded } = useUser();
-  const [userClerkId, setUserClerkId] = useState("");
-  const [isAuthStateLoading, setIsAuthStateLoading] = useState(true);
   const router = useRouter();
-
   const allUsersQuery = api.user.getAllUsers.useQuery();
   const allClerkUsersQuery = api.user.getAllClerkUsers.useQuery();
-  const currentUserQuery = api.user.findUserWithClerkId.useQuery(
-    userClerkId ?? "",
-  );
+
   const changeApprovalStatusMutation =
     api.user.changeApprovalStatus.useMutation();
 
   useEffect(() => {
-    if (user) {
-      setUserClerkId(user.id);
-    }
-  }, [user]);
-
-  // Clerk will resolve the user asynchronously, so we have to wait
-  // and see if the user is authorized. Once userClerkId is resolved
-  // useQuery will automatically refetch data with the new input
-  useEffect(() => {
     // user is not loaded, we're not sure if they're an admin yet
-    if (!isLoaded || !user || userClerkId === "") {
+    if (!isLoaded || !user) {
       return;
     }
 
-    // user is explicitly not an admin, not because api call missed
-    if (currentUserQuery.data?.isAdmin === false) {
+    if (user.publicMetadata.isAdmin !== true) {
       router.push("/");
-    } else if (currentUserQuery.data?.isAdmin === true) {
-      // user is explicitly an admin, we're no longer waiting for auth to resolve
-      setIsAuthStateLoading(false);
     }
-  }, [userClerkId, currentUserQuery, isLoaded, user, router]);
+  }, [user, isLoaded, router]);
 
   // we're trying to resolve the auth to determine if user has access to this page
-  if (!isLoaded || !user || isAuthStateLoading) return <div>Loading...</div>;
+  if (!isLoaded || !user) return <div>Loading...</div>;
 
   return (
     <div>
--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -4,16 +4,6 @@ import { z } from "zod";
 import { adminProcedure, createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
-  findUserWithClerkId: protectedProcedure
-    .input(z.string())
-    .query(async ({ input, ctx }) => {
-      return await ctx.db.user.findUniqueOrThrow({
-        where: {
-          clerkId: input,
-        },
-      });
-    }),
-
   getAllUsers: protectedProcedure.query(async ({ ctx }) => {
     return await ctx.db.user.findMany();
   }),
@@ -25,11 +15,9 @@ export const userRouter = createTRPCRouter({
     return new Map(users.map((user) => [user.id, user]));
   }),
 
-  // This will change the approval status of the user with the given id in our database
-  // If the user does not exist, they will NOT be created and an error will be thrown
-  //
-  // Although we can use the clerkId, we enforce fetching user the id in our database
-  // since the approval attribute is a concept associated with our user model rather than clerk
+  // This will change the approval status of the user with the given id in our database,
+  // and on clerk. When we retrieve this data we'll retrieve it from clerk, but we want
+  // to keep these two fields consistent in case we want to migrate off of clerk some day
   changeApprovalStatus: adminProcedure
     .input(
       z.object({
@@ -38,11 +26,16 @@ export const userRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ input, ctx }) => {
-      return await ctx.db.user.update({
+      const user = await ctx.db.user.update({
         where: {
           id: input.id,
         },
         data: {
+          isApproved: input.isApproved,
+        },
+      });
+      await clerkClient.users.updateUserMetadata(user.clerkId, {
+        publicMetadata: {
           isApproved: input.isApproved,
         },
       });
