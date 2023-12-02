7bd794f5a968dbff03bb78efd41a4226db5df7d8
Minh Le <ngocminhle318@gmail.com>
2023-12-02 17:15:03 -0500

    splash

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
+
+const splashImage = require("../../assets/landing_page.png");
+
+SplashScreen.preventAutoHideAsync();
 
 Notifications.setNotificationHandler({
   handleNotification: () =>
@@ -15,25 +20,88 @@ Notifications.setNotificationHandler({
     }),
 });
 
+// const Index = () => {
+//   const [, setExpoPushToken] = useState<Notifications.ExpoPushToken | null>(
+//     null,
+//   );
+//   const [appIsReady, setAppIsReady] = useState(false);
+
+//   useEffect(() => {
+
+//     registerForPushNotificationsAsync()
+//       .then((token) => setExpoPushToken(token ?? null))
+//       .catch((err) => console.error(err));
+//   }, []);
+//   return (
+//     <SafeAreaView className="">
+//       {/* Changes page title visible on the header */}
+//       <Stack.Screen options={{ title: "Home Page" }} />
+//       <View className="h-full w-full p-4">
+//         <Text> Index </Text>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default Index;
+
 const Index = () => {
   const [, setExpoPushToken] = useState<Notifications.ExpoPushToken | null>(
     null,
   );
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    registerForPushNotificationsAsync()
-      .then((token) => setExpoPushToken(token ?? null))
-      .catch((err) => console.error(err));
+    const prepare = async () => {
+      try {
+        // Register for push notifications
+        await registerForPushNotificationsAsync()
+          .then((token) => setExpoPushToken(token ?? null))
+          .catch((err) => console.error(err));
+
+        // Load the splash image
+        await Image.prefetch(splashImage);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    };
+
+    void prepare();
   }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <SafeAreaView className="">
-      {/* Changes page title visible on the header */}
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <Stack.Screen options={{ title: "Home Page" }} />
-      <View className="h-full w-full p-4">
+      <View style={styles.container}>
+        <Image source={splashImage} style={styles.splashImage} />
         <Text> Index </Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  splashImage: {
+    width: 500,
+    height: 700,
+  },
+});
 
 export default Index;
