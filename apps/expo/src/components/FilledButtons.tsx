3401ca2b5f98e1b5f257e868a1c1752f757944b6
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-01-21 15:21:49 -0500

    delete unused import

--- apps/expo/src/components/FilledButtons.tsx
+++ apps/expo/src/components/FilledButtons.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 interface FilledButtonProps {
   onClick: () => void;
