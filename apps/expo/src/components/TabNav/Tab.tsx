301e9d84fa6d40eb3418ca535fd34482e7149139
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-21 11:30:03 -0500

    format and lint

--- apps/expo/src/components/TabNav/Tab.tsx
+++ apps/expo/src/components/TabNav/Tab.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 
 interface TabProps {
-    label: string;
-    onChange?: () => void;
-    isActive?: boolean;
-    onPress?: () => void;
+  label: string;
+  onChange?: () => void;
+  isActive?: boolean;
+  onPress?: () => void;
 }
 
 const Tab: React.FC<TabProps> = ({ label, onChange, isActive, onPress }) => {
-    return (
-        <TouchableOpacity
-            onPress={() => {
-                if (onChange) onChange()
-                if (onPress) onPress()
-            }}
-            className={`flex-1 px-4 py-2 rounded-full ${isActive ? 'bg-p-80' : 'bg-p-99'}`}
-        >
-            <Text className={`text-sm text-p-0 font-title-md text-center`}>
-                {label}
-            </Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        if (onChange) onChange();
+        if (onPress) onPress();
+      }}
+      className={`flex-1 rounded-full px-4 py-2 ${
+        isActive ? "bg-p-80" : "bg-p-99"
+      }`}
+    >
+      <Text className={`text-p-0 font-title-md text-center text-sm`}>
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
 };
 
 export default Tab;
--- apps/expo/src/components/TabNav/TabNav.tsx
+++ apps/expo/src/components/TabNav/TabNav.tsx
@@ -1,32 +1,31 @@
-import type { ReactElement } from 'react';
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { styled } from 'nativewind';
-import Tab from './Tab';
+import type { ReactElement } from "react";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { styled } from "nativewind";
 
-const StyledView = styled(View)
+import Tab from "./Tab";
+
+const StyledView = styled(View);
 
 interface TabNavProps {
-    children: ReactElement<typeof Tab>[]
+  children: ReactElement<typeof Tab>[];
 }
 
 const TabNav: React.FC<TabNavProps> & { Tab: typeof Tab } = ({ children }) => {
-    const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
 
-    return (
-        <StyledView className="flex-row w-full bg-p-99 rounded-full">
-            {
-                React.Children.map(children, (child, index) =>
-                    React.cloneElement(child as React.ReactElement, {
-                        isActive: index === activeTab,
-                        onChange: () => setActiveTab(index),
-                    }),
-                )
-            }
-        </StyledView>
-    );
+  return (
+    <StyledView className="bg-p-99 w-full flex-row rounded-full">
+      {React.Children.map(children, (child, index) =>
+        React.cloneElement(child as React.ReactElement, {
+          isActive: index === activeTab,
+          onChange: () => setActiveTab(index),
+        }),
+      )}
+    </StyledView>
+  );
 };
 
-TabNav.Tab = Tab
+TabNav.Tab = Tab;
 
 export default TabNav;
