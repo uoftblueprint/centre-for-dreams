5ef2cd4f00d30f90b36f5720bfe4f2a0120d046b
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-01-28 16:24:01 -0500

    change prop name to make more sense

--- apps/expo/src/components/OutlinedButtons.tsx
+++ apps/expo/src/components/OutlinedButtons.tsx
@@ -4,21 +4,21 @@ import { Text, TouchableOpacity, View } from "react-native";
 interface OutlinedButtonProps {
   onClick: () => void;
   children: React.ReactNode;
-  dark_blue: boolean;
+  fill: boolean;
   // icon to be added
 }
 
 const OutlinedButton: React.FC<OutlinedButtonProps> = ({
   onClick,
   children,
-  dark_blue,
+  fill,
 }) => {
   return (
     <TouchableOpacity onPress={onClick}>
       <View className="h-12 items-start gap-2">
         <View
           className={`border-p-40 rounded-full border bg-white px-6 py-2.5 ${
-            dark_blue ? "bg-p-80" : "bg-white"
+            fill ? "bg-p-80" : "bg-white"
           }`}
         >
           <Text className="font-title-md text-p-40">{children}</Text>
