0a833a69c5bd36683eab7f9ef1c4363aaa997c56
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-01-31 19:34:34 -0500

    add: option to add icon to the right

--- apps/expo/src/components/inputs/TextInput.tsx
+++ apps/expo/src/components/inputs/TextInput.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { TextInput as UnstyledTextInput } from 'react-native';
-import type { TextInputProps } from 'react-native';
+import { TextInput as UnstyledTextInput, View } from 'react-native';
+import type { TextInputProps, ViewProps } from 'react-native';
 
 type CustomTextInputProps = TextInputProps
 
-const TextInput: React.FC<CustomTextInputProps> = (props) => {
+export const TextInput: React.FC<CustomTextInputProps> & { RightElement: typeof RightElement } = ({ children, ...props }) => {
     return (
-        <UnstyledTextInput
-            className="border border-p-40 rounded-[8px] p-[12px] font-body-lg leading-[0px] text-p-0"
-            placeholderTextColor={"#79767D"}
-            {...props}
-        />
+        <View className="position-relative">
+            <UnstyledTextInput
+                className="border border-p-40 rounded-[8px] p-[12px] font-body-lg leading-[0px] text-p-0"
+                placeholderTextColor={"#79767D"}
+                {...props}
+            />
+            {children}
+        </View>
     );
 };
 
-export default TextInput;
+interface RightElementProps extends ViewProps {
+    children: React.ReactNode;
+}
+
+const RightElement: React.FC<RightElementProps> = ({ children, ...props }) => {
+    return (
+        <View className="absolute right-[12px] top-[8px]" {...props}>
+            {children}
+        </View>
+    );
+}
+
+TextInput.RightElement = RightElement;
