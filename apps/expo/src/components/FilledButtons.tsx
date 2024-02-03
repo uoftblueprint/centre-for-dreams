4aa0ba2dde7c9fbc22ff0a8a5a5f135e4564c45d
RyanL123 <ryan.lichangrui@gmail.com>
2024-02-03 17:52:25 -0500

    fix lint errors

--- apps/expo/src/components/FilledButtons.tsx
+++ apps/expo/src/components/FilledButtons.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
 interface FilledButtonProps {
   onClick: () => void;
