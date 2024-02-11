fe278696014139841e25028a8b137d23a85abc50
Helena Glowacki <113125436+helenaglow@users.noreply.github.com>
2024-02-10 21:19:21 -0500

    changes

--- apps/expo/src/components/EventTab.tsx
+++ apps/expo/src/components/EventTab.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-import type { RouterOutputs } from "~/utils/api";
-
-type GetActivityOutput = RouterOutputs["activity"]["getSchedule"][number];
+// TO-DO : Change to be Activity type when structure is ready
+export interface Event {
+  name: string;
+}
 
 interface EventProps {
-  activity: GetActivityOutput;
+  activity: Event;
   attending?: boolean;
 }
 
 export default function EventTab({ activity, attending }: EventProps) {
   return (
     <View className={`w-fill bg-p-95 flex-col justify-center rounded-md p-4`}>
-      <Text className=" text-p-10 font-poppins500 text-base">
-        {activity.name}
-      </Text>
+      <Text className="text-p-10 font-title-md">{activity.name}</Text>
 
       <View className="flex-row items-center justify-start">
-        <Text className="text-n-40 font-poppins400 mr-3 text-sm">
-          Attendance:
-        </Text>
+        <Text className="text-n-40 font-body-md mr-3">Attendance:</Text>
 
+        {/*TO-DO: Change to be chips when they are ready */}
         <Text className="text-base font-normal">
           {attending === undefined ? "Maybe" : attending ? "Join" : "Not Join"}
         </Text>
