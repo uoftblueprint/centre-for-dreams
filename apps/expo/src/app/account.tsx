b5aa16c7259994caa487e9502a0de09addc91272
RyanL123 <ryan.lichangrui@gmail.com>
2024-02-14 22:02:41 -0500

    skeleton

--- apps/expo/src/app/account.tsx
+++ apps/expo/src/app/account.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
@@ -7,10 +7,27 @@ import SignOut from "~/components/SignOut";
 
 const Account = () => {
   return (
-    <SafeAreaView className="">
+    <SafeAreaView className="flex">
       <Stack.Screen options={{ title: "Account", headerShown: false }} />
-      <Text>Account</Text>
-      <SignOut />
+      <View className="bg-red-400 p-4">
+        <Text>Account</Text>
+      </View>
+      <View className=" bg-blue-400 p-4">
+        <View className="rounded-md bg-purple-400 p-2">
+          <View className="bg-slate-400 object-cover p-2">
+            <Image
+              style={{ width: 50, height: 50 }}
+              source={{
+                uri: "https://www.opensesame.com/wp-content/uploads/2018/12/Tencent-QQ-logo.png",
+              }}
+            />
+          </View>
+          <Text>Profile</Text>
+        </View>
+      </View>
+      <View className="bg-green-400 p-4">
+        <Text>Push Notifications</Text>
+      </View>
     </SafeAreaView>
   );
 };
