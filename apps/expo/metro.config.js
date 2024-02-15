391c0d3edad233a803e46e70c4f5f1f158b29e41
Baker Jackson <bakerwjackson@gmail.com>
2024-02-14 18:43:08 -0800

    Implement react-native-svg-transformer (#365)

--- apps/expo/metro.config.js
+++ apps/expo/metro.config.js
@@ -9,6 +9,7 @@ const workspaceRoot = path.resolve(projectRoot, "../..");
 const config = getDefaultConfig(projectRoot);
 
 if (config.resolver) {
+  const { transformer, resolver } = config;
   // 1. Watch all files within the monorepo
   config.watchFolders = [workspaceRoot];
   // 2. Let Metro know where to resolve packages and in what order
@@ -18,6 +19,20 @@ if (config.resolver) {
   ];
   // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
   config.resolver.disableHierarchicalLookup = true;
+
+  // @ts-ignore
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  // @ts-ignore
+  config.resolver = {
+    ...resolver,
+    // @ts-ignore
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    // @ts-ignore
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
 }
 
 module.exports = config;
