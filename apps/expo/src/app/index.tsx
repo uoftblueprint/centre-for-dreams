9b2276a85f08e0059b1fcfa92962c5cffc0b5578
Baker Jackson <bakerwjackson@gmail.com>
2024-02-14 19:45:42 -0500

    revert change to index.tsx

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -3,7 +3,6 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
-import A from '../../assets/a.svg';
 
 import registerForPushNotificationsAsync from "~/notifications/registerNotifications";
 
@@ -33,7 +32,6 @@ const Index = () => {
       <Stack.Screen options={{ title: "Home Page" }} />
       <View className="h-full w-full p-4">
         <Text className="font-poppins400"> Index </Text>
-        <A />
       </View>
     </SafeAreaView>
   );
