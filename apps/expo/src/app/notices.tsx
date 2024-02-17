8db87ae983045e3534d04ff8c63c08d5eb88e485
Ram Raghav Sharma <92739966+Ram-Raghav-S@users.noreply.github.com>
2024-02-17 02:50:48 -0500

    Announcement page and card  (#366)

--- apps/expo/src/app/notices.tsx
+++ apps/expo/src/app/notices.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 
+import AnnouncementCard from "~/components/AnnouncementCard";
+import { api } from "~/utils/api";
+import Filter from "../../assets/filter.svg";
+
 const Notices = () => {
+  // TODO: fix padding issue with NavBar
+  const announcements = api.announcement.getAnnouncements.useQuery();
   return (
-    <SafeAreaView className="">
+    <SafeAreaView className="bg-white">
       <Stack.Screen options={{ title: "Notices", headerShown: false }} />
-      <Text>Notices</Text>
+
+      <View className="px-5 pb-[10%]">
+        <View className="flex-row items-center justify-between pb-2 pt-6">
+          <View className="w-24px h-24px" />
+          <Text className="font-headline-md text-center">Announcement</Text>
+          <Filter width={24} height={24} />
+        </View>
+        <FlatList
+          data={announcements.data}
+          renderItem={({ item }) => <AnnouncementCard announcement={item} />}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <View className="h-4" />}
+        />
+      </View>
     </SafeAreaView>
   );
 };
