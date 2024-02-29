e09d8c530601d8e21a87ee74ecc44ae6e213e0ee
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-02-28 20:56:14 -0500

    add: server side procedure calling

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,9 +1,11 @@
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
+import { appRouter } from "./src/root";
 import type { AppRouter } from "./src/root";
+import { createCallerFactory } from "./src/trpc";
 
 export { appRouter, type AppRouter } from "./src/root";
-export { createTRPCContext } from "./src/trpc";
+export { createTRPCContext, createCallerFactory } from "./src/trpc";
 
 /**
  * Inference helpers for input types
@@ -16,3 +18,5 @@ export type RouterInputs = inferRouterInputs<AppRouter>;
  * @example type HelloOutput = RouterOutputs['example']['hello']
  **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
+
+export const createCaller = createCallerFactory(appRouter);
