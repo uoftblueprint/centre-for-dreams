82dd5681b3b58aad4d0bf0cacb2c746a0660dc94
Helena Glowacki <113125436+helenaglow@users.noreply.github.com>
2024-03-02 23:25:00 -0500

    [CFD-98] Add icons to the navbar component (#392)

--- apps/expo/src/components/NavBar.tsx
+++ apps/expo/src/components/NavBar.tsx
@@ -1,11 +1,28 @@
+import type { ReactElement } from "react";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { Tabs } from "expo-router";
+
+import CalendarFocused from "../../assets/calendar-white.svg";
+import Calendar from "../../assets/calendar.svg";
+import ForumFocused from "../../assets/chatbubble-white.svg";
+import Forum from "../../assets/chatbubble.svg";
+import NoticesFocused from "../../assets/speakerphone-white.svg";
+import Notices from "../../assets/speakerphone.svg";
+import AccountFocused from "../../assets/user-white.svg";
+import Account from "../../assets/user.svg";
+
+const icons: Record<string, ReactElement[]> = {
+  calendar: [<Calendar key="normal" />, <CalendarFocused key="focused" />],
+  notices: [<Notices key="normal" />, <NoticesFocused key="focused" />],
+  forum: [<Forum key="normal" />, <ForumFocused key="focused" />],
+  account: [<Account key="normal" />, <AccountFocused key="focused" />],
+};
 
 const NavBar = () => {
   return (
     <Tabs
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: {
           height: "10%",
@@ -24,19 +41,15 @@ const NavBar = () => {
         tabBarIcon: ({ focused }) => (
           <View
             className={`top-2 h-9 w-[68px] flex-1 items-center justify-center ${
-              focused ? "rounded-[22px] bg-white" : ""
+              focused ? "bg-p-40 rounded-[22px]" : ""
             }`}
           >
             <View className="h-9 w-9 items-center justify-center">
-              {/* Add icon here for tab */}
-              <Text className={`font-poppins500 text-p-40 text-sm font-medium`}>
-                {" "}
-                ?{" "}
-              </Text>
+              {focused ? icons[route.name]![1] : icons[route.name]![0]}
             </View>
           </View>
         ),
-      }}
+      })}
     >
       <Tabs.Screen
         // Name of the route to hide.
