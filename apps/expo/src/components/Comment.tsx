213a4afd41ea813d204c70faa4a7927870f879da
Sarina Li <74844953+sarinali@users.noreply.github.com>
2024-03-02 21:50:30 -0500

    Comments layout

--- apps/expo/src/components/Comment.tsx
+++ apps/expo/src/components/Comment.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 import type { RouterOutputs } from "~/utils/api";
 import { formatTime } from "~/utils/dateformatter";
@@ -7,8 +7,19 @@ import { formatTime } from "~/utils/dateformatter";
 type CommentComponent = RouterOutputs["comment"]["create"];
 
 export default function Comment({ comment }: { comment: CommentComponent }) {
+  const [showFullText, setShowFullText] = useState(false);
+
+  const toggleShowFullText = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const truncatedText =
+    comment.text.length > 200
+      ? comment.text.slice(0, 200) + "... "
+      : comment.text;
+
   return (
-    <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-6">
+    <View className="h-25 mx-auto w-11/12 rounded-lg bg-white pb-2 pr-2 pt-2">
       <View className="flex-row items-center justify-start">
         <Image
           source={{
@@ -18,14 +29,24 @@ export default function Comment({ comment }: { comment: CommentComponent }) {
           resizeMode="cover"
         />
         <View>
-          <Text className="font-title-md ml-4">John Doe</Text>
+          <Text className="font-title-md ml-4">{comment.userId}</Text>
           <Text className="font-body-md text-n-60 ml-4">
             {formatTime(comment.createdAt)}
           </Text>
         </View>
       </View>
-
-      <Text className="font-body-md ml-16 mt-2">{comment.text}</Text>
+      <View>
+        <Text className="font-body-md ml-16 mt-2">
+          {showFullText ? comment.text : truncatedText}
+          {comment.text.length > 200 && (
+            <TouchableOpacity onPress={toggleShowFullText}>
+              <Text className="color-t-0 underline">
+                {showFullText ? " See Less" : "See More"}
+              </Text>
+            </TouchableOpacity>
+          )}
+        </Text>
+      </View>
     </View>
   );
 }
