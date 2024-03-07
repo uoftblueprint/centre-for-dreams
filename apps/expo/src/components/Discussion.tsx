66cd71e0a86e4e575e5a8609bf601bcc169ef8dd
Sarina Li <74844953+sarinali@users.noreply.github.com>
2024-03-07 13:09:26 -0500

    Implement Updated Forum Post Design

--- apps/expo/src/components/Discussion.tsx
+++ apps/expo/src/components/Discussion.tsx
@@ -3,8 +3,10 @@ import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 
 import type { RouterOutputs } from "~/utils/api";
 import { formatTime } from "~/utils/dateformatter";
+import CommentIconBlue from "../../assets/comment-blue.svg";
 import CommentIcon from "../../assets/comment.svg";
 import EditIcon from "../../assets/edit.svg";
+import LikeIconBlue from "../../assets/like-blue.svg";
 import LikeIcon from "../../assets/like.svg";
 import Comment from "./Comment";
 
@@ -40,7 +42,7 @@ export default function Discussion({
       {/* Display "View More" button after the first comment */}
       {index === 0 && discussion.comments.length > 1 && !showMoreComments && (
         <TouchableOpacity onPress={handleViewMore}>
-          <Text className=" color-t-0 ml-3 underline">
+          <Text className=" color-t-0 font-body-md mb-3 ml-3 underline">
             {"View all " + totalComments.toString() + " comments"}
           </Text>
         </TouchableOpacity>
@@ -51,45 +53,59 @@ export default function Discussion({
   );
 
   return (
-    <View className="h-25 mx-auto w-11/12 rounded-lg bg-white p-4">
-      <View className="mt-2 flex-row items-center">
-        <Image
-          source={{
-            uri: "https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857",
-          }}
-          className="h-12 w-12 rounded-full"
-          resizeMode="cover"
-        />
-        <View>
-          <Text className="font-title-lg ml-4 mr-4">{discussion.title}</Text>
-          <Text className="font-body-lg ml-4">
-            posted {formatTime(discussion.createdAt)}
-          </Text>
-        </View>
-      </View>
-      <View>
-        <Text className="font-body-lg mt-4">{discussion.contents}</Text>
-        <View className="mt-2">
+    <View className="h-25 mx-auto w-11/12">
+      <View className="rounded-lg bg-white p-4">
+        <View className="mt-2 flex-row items-center ">
           <Image
             source={{
               uri: "https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857",
             }}
-            className="h-60 w-fit"
+            className="h-12 w-12 rounded-full"
+            resizeMode="cover"
           />
+          <View>
+            <Text className="font-title-lg ml-4 mr-4">{discussion.title}</Text>
+            <Text className="font-body-lg ml-4">
+              posted {formatTime(discussion.createdAt)}
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text className="font-body-lg mt-4">{discussion.contents}</Text>
+          <View className="mt-2">
+            <Image
+              source={{
+                uri: "https://static.wikia.nocookie.net/acc-official-database/images/9/91/El_gato.jpg/revision/latest?cb=20220709001857",
+              }}
+              className="h-60 w-fit"
+            />
+          </View>
+        </View>
+        <View className="mt-2 flex-row">
+          <LikeIconBlue
+            width={18}
+            height={20}
+            className="color-p-60"
+          ></LikeIconBlue>
+          <Text className="font-body-lg text-n-50 ml-2">2</Text>
+          <View className="w-2"></View>
+          <CommentIconBlue width={16} height={20}></CommentIconBlue>
+          <Text className="font-body-lg text-n-50 ml-2">
+            {discussion.comments.length}
+          </Text>
         </View>
       </View>
+
       {canEdit && (
-        <View className="mt-2 flex-row items-center justify-center p-2">
+        <View className="mb-4 mt-4 flex-row items-center justify-center p-2">
           <View className="w-1/3 flex-row justify-center">
             <LikeIcon width={15} height={18}></LikeIcon>
             <Text className="font-body-md ml-2">Like</Text>
           </View>
-          <View className="h-10 w-px bg-gray-300" />
           <View className="w-5/12 flex-row justify-center">
             <CommentIcon width={13} height={18}></CommentIcon>
             <Text className="font-body-md ml-2">Comment</Text>
           </View>
-          <View className="h-10 w-px bg-gray-300" />
           <View className="w-1/3 flex-row justify-center">
             <EditIcon width={13} height={18}></EditIcon>
             <Text className="font-body-md ml-2">Edit</Text>
@@ -97,31 +113,36 @@ export default function Discussion({
         </View>
       )}
       {!canEdit && (
-        <View className="mt-2 flex-row items-center justify-center p-2">
+        <View className="mb-4 mt-4 flex-row items-center justify-center p-2">
           <View className="w-1/2 flex-row justify-center">
             <LikeIcon width={15} height={18}></LikeIcon>
             <Text className="font-body-md ml-2">Like</Text>
           </View>
-          <View className="h-10 w-px bg-gray-300" />
           <View className="w-1/2 flex-row justify-center">
             <CommentIcon width={13} height={18}></CommentIcon>
             <Text className="font-body-md ml-2">Comment</Text>
           </View>
         </View>
       )}
-      <FlatList
-        data={discussion.comments?.slice().reverse()}
-        renderItem={({ item, index }) =>
-          renderItem({ item, index, totalComments: discussion.comments.length })
-        }
-        keyExtractor={(item) => item.id.toString()}
-      />
-      {/* Display "View Less" button if "View More" was pressed */}
-      {showMoreComments && (
-        <TouchableOpacity onPress={handleViewLess}>
-          <Text className="color-t-0 ml-3 mt-10 underline">View Less</Text>
-        </TouchableOpacity>
-      )}
+      <View className="rounded-lg bg-white">
+        <FlatList
+          data={discussion.comments?.slice().reverse()}
+          renderItem={({ item, index }) =>
+            renderItem({
+              item,
+              index,
+              totalComments: discussion.comments.length,
+            })
+          }
+          keyExtractor={(item) => item.id.toString()}
+        />
+        {/* Display "View Less" button if "View More" was pressed */}
+        {showMoreComments && (
+          <TouchableOpacity onPress={handleViewLess}>
+            <Text className="color-t-0 mb-3 ml-3 underline">View Less</Text>
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 }
