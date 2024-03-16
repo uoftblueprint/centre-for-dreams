d71d045f6069b145dd53cad01ea47a54fc186733
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-03-16 15:59:00 -0400

    add: likePost and unlikePost endpoints

---
+++ packages/api/src/router/like.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const likeRouter = createTRPCRouter({
+  likePost: protectedProcedure
+    .input(
+      z.object({
+        postId: z.number().nonnegative(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.likes.create({
+        data: {
+          postId: input.postId,
+          userId: ctx.userId,
+        },
+      });
+    }),
+
+  unlikePost: protectedProcedure
+    .input(
+      z.object({
+        postId: z.number().nonnegative(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.likes.deleteMany({
+        where: {
+          postId: input.postId,
+          userId: ctx.userId,
+        },
+      });
+    }),
+});
