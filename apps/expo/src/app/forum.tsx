79d24785a8203a1e1bec81a3cbb4037a963b148f
Minh Le <ngocminhle318@gmail.com>
2024-03-20 12:23:16 -0400

    fix

--- apps/expo/src/app/forum.tsx
+++ apps/expo/src/app/forum.tsx
@@ -38,32 +38,9 @@ const renderHeader = () => (
 const Forum = () => {
   const forumPosts = api.discussion.getDiscussions.useQuery().data;
   return (
-    <SafeAreaView className="bg-p-100 flex-1">
-      <ScrollView>
-        <Stack.Screen options={{ title: "Forum", headerShown: false }} />
-
-        <View className="mt-10 flex-1 items-center justify-center">
-          <Text className=" font-display-sm">Forum</Text>
-        </View>
-        <View className="ml-4 mr-4 mt-4 flex-1 items-center justify-center">
-          <TabNav>
-            <TabNav.Tab onPress={() => console.log("Tab 1 pressed")}>
-              Feed
-            </TabNav.Tab>
-            <TabNav.Tab onPress={() => console.log("Tab 2 pressed")}>
-              My Posts
-            </TabNav.Tab>
-            <TabNav.Tab onPress={() => console.log("Tab 3 pressed")}>
-              <View className="relative">
-                {/* <SvgXml xml={Bell} width={20} height={20} /> */}
-                <Bell width={20} height={20}></Bell>
-                <View className="absolute left-3.5 top-0 ">
-                  <RedDot width={6} height={6} />
-                </View>
-              </View>
-            </TabNav.Tab>
-          </TabNav>
-        </View>
+    <SafeAreaView className="">
+      <Stack.Screen options={{ title: "Forum", headerShown: false }} />
+      <View className="mb-16">
         <FlatList
           data={forumPosts}
           ListHeaderComponent={renderHeader}
