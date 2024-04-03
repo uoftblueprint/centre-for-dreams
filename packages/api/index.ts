da3a93fefa833a5df6c9f489d25e6b82b7aa5274
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2024-04-03 15:43:02 -0400

    Revert "add: server side procedure calling"
    This reverts commit dfe82005e317e32cbc28b2f47d5a507add7396a3.

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,11 +1,9 @@
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
-import { appRouter } from "./src/root";
 import type { AppRouter } from "./src/root";
-import { createCallerFactory } from "./src/trpc";
 
 export { appRouter, type AppRouter } from "./src/root";
-export { createTRPCContext, createCallerFactory } from "./src/trpc";
+export { createTRPCContext } from "./src/trpc";
 
 /**
  * Inference helpers for input types
@@ -18,5 +16,3 @@ export type RouterInputs = inferRouterInputs<AppRouter>;
  * @example type HelloOutput = RouterOutputs['example']['hello']
  **/
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
-
-export const createCaller = createCallerFactory(appRouter);
