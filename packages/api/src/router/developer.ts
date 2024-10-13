348d3441efb2683969e47ddce20f0dcba16d5dea
Dani3lx <1danielxu@gmail.com>
2024-10-13 10:58:58 -0400

    Implemented Card for Nextjs

--- packages/api/src/router/developer.ts
+++ packages/api/src/router/developer.ts
@@ -82,4 +82,37 @@ export const developerRouter = createTRPCRouter({
       data: { upvotes: { increment: 1 } },
     });
   }),
+
+  danielxu_info: publicProcedure.query(async ({ ctx }) => {
+    let developer = await ctx.db.developers.findFirst({
+      where: { name: "Daniel Xu" },
+    });
+
+    // Create developer if doesn't exist
+    if (developer === null) {
+      developer = await ctx.db.developers.create({
+        data: {
+          name: "Daniel Xu",
+          upvotes: 0,
+        },
+      });
+    }
+
+    return {
+      name: "Daniel Xu",
+      year: 4,
+      introduction:
+        "I'm a senior developer on the team, and I playn't Pokemon Go.",
+      fav_food: "Sushi",
+      fav_song: "Cherry Wine by Grentperez",
+      upvotes: developer.upvotes,
+    };
+  }),
+
+  danielxu_update: publicProcedure.mutation(async ({ ctx }) => {
+    return await ctx.db.developers.update({
+      where: { name: "Daniel Xu" },
+      data: { upvotes: { increment: 1 } },
+    });
+  }),
 });
