62f3c6c218d5ddb7ac0a27dc08779fa89b6c0e9e
Nolawi Teklehaimanot <110188368+nolawiyonas1@users.noreply.github.com>
2024-10-30 10:22:31 -0400

    starter task 2

--- apps/expo/src/components/developers/Nolawi.tsx
+++ apps/expo/src/components/developers/Nolawi.tsx
@@ -4,53 +4,53 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { api } from "~/utils/api";
 
 const Nolawi = () => {
-    const { data: nolawiInfo } = api.developer.nolawi_info.useQuery();
-    const mutation = api.developer.nolawi_upvote.useMutation();
+  const { data: nolawiInfo } = api.developer.nolawi_info.useQuery();
+  const mutation = api.developer.nolawi_upvote.useMutation();
 
-    return (
-        <View className="m-4 rounded-lg border-2 border-blue-500 bg-white p-6 shadow-lg">
-            <View className="mb-4">
-                <Text className="text-2xl font-semibold tracking-tight text-gray-900">
-                    {nolawiInfo?.name}
-                </Text>
-            </View>
+  return (
+    <View className="m-4 rounded-lg border-2 border-blue-500 bg-white p-6 shadow-lg">
+      <View className="mb-4">
+        <Text className="text-2xl font-semibold tracking-tight text-gray-900">
+          {nolawiInfo?.name}
+        </Text>
+      </View>
 
-            <View className="mb-4">
-                <Text className="text-md">Year: {nolawiInfo?.year}</Text>
-            </View>
+      <View className="mb-4">
+        <Text className="text-md">Year: {nolawiInfo?.year}</Text>
+      </View>
 
-            <View className="mb-4">
-                <Text>{nolawiInfo?.introduction}</Text>
-            </View>
+      <View className="mb-4">
+        <Text>{nolawiInfo?.introduction}</Text>
+      </View>
 
-            <View className="mb-4">
-                <Text>
-                    <Text className="font-bold">Favourite food: </Text>
-                    {nolawiInfo?.fav_food}
-                </Text>
-            </View>
+      <View className="mb-4">
+        <Text>
+          <Text className="font-bold">Favourite food: </Text>
+          {nolawiInfo?.fav_food}
+        </Text>
+      </View>
 
-            <View className="mb-4">
-                <Text>
-                    <Text className="font-bold">Favourite song: </Text>
-                    {nolawiInfo?.fav_song}
-                </Text>
-            </View>
+      <View className="mb-4">
+        <Text>
+          <Text className="font-bold">Favourite song: </Text>
+          {nolawiInfo?.fav_song}
+        </Text>
+      </View>
 
-            <View className="mt-4 flex-row items-center gap-4">
-                <TouchableOpacity
-                    className="rounded bg-blue-500 px-4 py-2"
-                    onPress={() => mutation.mutate()}
-                >
-                    <Text className="font-bold text-white">Upvote</Text>
-                </TouchableOpacity>
+      <View className="mt-4 flex-row items-center gap-4">
+        <TouchableOpacity
+          className="rounded bg-blue-500 px-4 py-2"
+          onPress={() => mutation.mutate()}
+        >
+          <Text className="font-bold text-white">Upvote</Text>
+        </TouchableOpacity>
 
-                <View className="pt-2">
-                    <Text className="font-bold">Upvotes: {nolawiInfo?.upvotes}</Text>
-                </View>
-            </View>
+        <View className="pt-2">
+          <Text className="font-bold">Upvotes: {nolawiInfo?.upvotes}</Text>
         </View>
-    );
+      </View>
+    </View>
+  );
 };
 
 export default Nolawi;
--- packages/api/src/router/developer.ts
+++ packages/api/src/router/developer.ts
@@ -200,7 +200,7 @@ export const developerRouter = createTRPCRouter({
       },
     });
   }),
-  
+
   nolawi_info: publicProcedure.query(async ({ ctx }) => {
     let upvotes = 0;
     // check if exist in table
