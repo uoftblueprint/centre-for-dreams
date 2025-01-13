4a48b08c274c6f126169e89ec01548c189dc8364
elsiezhu <elsie.zhu2003@gmail.com>
2025-01-12 21:16:26 -0500

    fix prettier errors

--- apps/nextjs/src/pages/posts.tsx
+++ apps/nextjs/src/pages/posts.tsx
@@ -4,39 +4,146 @@ import { SignInButton, useAuth, UserButton } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import Post from "../components/post";
 
-
 function Posts() {
   const posts = api.discussion.getDiscussions.useQuery();
 
   const { isSignedIn } = useAuth();
   return (
-    <div className="flex" style={{display: "flex", position: "absolute", width: "100%", top: "0px", bottom: "0px"}}>
-      <div 
-        className="m-10 flex" 
-        style={{flexDirection: "column", background: "#EFF2FB", margin: "0", padding: "16px 40px", maxWidth: "40%", justifyContent: "space-between", position: "sticky", top: 0}}
+    <div
+      className="flex"
+      style={{
+        display: "flex",
+        position: "absolute",
+        width: "100%",
+        top: "0px",
+        bottom: "0px",
+      }}
+    >
+      <div
+        className="m-10 flex"
+        style={{
+          flexDirection: "column",
+          background: "#EFF2FB",
+          margin: "0",
+          padding: "16px 40px",
+          maxWidth: "40%",
+          justifyContent: "space-between",
+          position: "sticky",
+          top: 0,
+        }}
       >
-        {isSignedIn ? "" : <SignInButton  />}
-        <UserButton afterSignOutUrl="/" showName/>
-        <nav style={{display: "flex", flexDirection: "column"}}>
-          <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px", backgroundColor: "#2E4D90", color: "#FFFFFF"}}><a href="/posts/" >Forum</a></button>
-          <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px"}}><a href="/announcements/">Announcements</a></button>
-          <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px"}}><a href="/absences/">Absentees</a></button>
-          <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px"}}><a href="/activities/">Calendar</a></button>
-          <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px"}}><a href="/activities/">Invite New User</a></button>
+        {isSignedIn ? "" : <SignInButton />}
+        <UserButton afterSignOutUrl="/" showName />
+        <nav style={{ display: "flex", flexDirection: "column" }}>
+          <button
+            style={{
+              borderWidth: "1px",
+              borderColor: "#2E4D90",
+              borderRadius: "24px",
+              padding: "8px",
+              margin: "8px",
+              backgroundColor: "#2E4D90",
+              color: "#FFFFFF",
+            }}
+          >
+            <a href="/posts/">Forum</a>
+          </button>
+          <button
+            style={{
+              borderWidth: "1px",
+              borderColor: "#2E4D90",
+              borderRadius: "24px",
+              padding: "8px",
+              margin: "8px",
+            }}
+          >
+            <a href="/announcements/">Announcements</a>
+          </button>
+          <button
+            style={{
+              borderWidth: "1px",
+              borderColor: "#2E4D90",
+              borderRadius: "24px",
+              padding: "8px",
+              margin: "8px",
+            }}
+          >
+            <a href="/absences/">Absentees</a>
+          </button>
+          <button
+            style={{
+              borderWidth: "1px",
+              borderColor: "#2E4D90",
+              borderRadius: "24px",
+              padding: "8px",
+              margin: "8px",
+            }}
+          >
+            <a href="/activities/">Calendar</a>
+          </button>
+          <button
+            style={{
+              borderWidth: "1px",
+              borderColor: "#2E4D90",
+              borderRadius: "24px",
+              padding: "8px",
+              margin: "8px",
+            }}
+          >
+            <a href="/activities/">Invite New User</a>
+          </button>
         </nav>
-        <button style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", padding: "8px", margin: "8px"}}><a href="/activities/">Create New</a></button>
+        <button
+          style={{
+            borderWidth: "1px",
+            borderColor: "#2E4D90",
+            borderRadius: "24px",
+            padding: "8px",
+            margin: "8px",
+          }}
+        >
+          <a href="/activities/">Create New</a>
+        </button>
       </div>
-    <div style={{display: "flex", flexDirection: "column", alignContent: "center", width: "-webkit-fill-available", paddingTop: "24px"}}>
-      <div style={{borderWidth: "1px", borderColor: "#2E4D90", borderRadius: "24px", margin: "8px", width: "max-content", alignSelf: "center"}}>
-        <button style={{borderRadius: "24px", padding: "8px 40px 8px 40px", backgroundColor: "#2E4D90", color: "#FFFFFF"}}>All Posts</button>
-        <button style={{borderRadius: "24px", padding: "8px 40px 8px 40px"}}>My Posts</button>
-      </div>
-      {posts.data?.map((p) => { 
-        // get user name from id and pass it in
-        return <Post key={p.id} {...p} />
-        })
-      }
-
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "center",
+          width: "-webkit-fill-available",
+          paddingTop: "24px",
+        }}
+      >
+        <div
+          style={{
+            borderWidth: "1px",
+            borderColor: "#2E4D90",
+            borderRadius: "24px",
+            margin: "8px",
+            width: "max-content",
+            alignSelf: "center",
+          }}
+        >
+          <button
+            style={{
+              borderRadius: "24px",
+              padding: "8px 40px 8px 40px",
+              backgroundColor: "#2E4D90",
+              color: "#FFFFFF",
+            }}
+          >
+            All Posts
+          </button>
+          <button
+            style={{ borderRadius: "24px", padding: "8px 40px 8px 40px" }}
+          >
+            My Posts
+          </button>
+        </div>
+        {posts.data?.map((p) => {
+          // get user name from id and pass it in
+          return <Post key={p.id} {...p} />;
+        })}
       </div>
     </div>
   );
