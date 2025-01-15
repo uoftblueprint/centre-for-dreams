c2a7fc2fed9a8b722a5ff2d01cd9b3f7ca1bdd10
Dani3lx <1danielxu@gmail.com>
2025-01-15 16:41:43 -0500

    Implemented UI

--- apps/nextjs/src/pages/activities.tsx
+++ apps/nextjs/src/pages/activities.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import CreateActivity from "../components/activity/activityform";
 import ViewActivities from "../components/activity/viewactivites";
+import ActivityModal from "~/components/activity/activitymodal";
 
 const Activities = () => {
   return (
-    <div className="h-full w-full p-4">
-      <div className="font-medium">Create Activity</div>
-      <CreateActivity />
-      <div className="font-medium">View Activites</div>
-      <ViewActivities />
-    </div>
+    // <div className="h-full w-full p-4">
+    //   <div className="font-medium">Create Activity</div>
+    //   <CreateActivity />
+    //   <div className="font-medium">View Activites</div>
+    //   <ViewActivities />
+    // </div>
+    <ActivityModal />
   );
 };
 
