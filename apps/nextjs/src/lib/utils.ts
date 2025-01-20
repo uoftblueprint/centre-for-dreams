bb4a70b26ae3a018d3fc214dc91311022fdf0331
elsiezhu <elsie.zhu2003@gmail.com>
2025-01-19 21:40:13 -0500

    run linter and formatter

--- apps/nextjs/src/lib/utils.ts
+++ apps/nextjs/src/lib/utils.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/consistent-type-specifier-style */
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -3,18 +3,18 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@cfd/tailwind-config";
 
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{ts,tsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
   presets: [baseConfig],
-    plugins: [require("tailwindcss-animate")],
-    theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    }
+  plugins: [require("tailwindcss-animate")],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
 } satisfies Config;
