757b61a24072869d7e8a9f622d38550b62ecab66
elsiezhu <elsie.zhu2003@gmail.com>
2025-01-19 21:38:23 -0500

    implement absentees table

---
+++ apps/nextjs/src/components/absenteestable.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import { cn } from "~/lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "./ui/calendar";
+import { api } from "~/utils/api";
+
+const selectedDateStyle = {
+  backgroundColor: "#2E4D90",
+  color: "#FFFFFF",
+  borderRadius: "24px"
+};
+
+const noSelectedDateStyle = {
+  borderWidth: "1px",
+  borderColor: "#2E4D90",
+  borderRadius: "24px",
+};
+
+export default function AbsenteesTable() {
+  const [selectedDate, setSelectedDate] = React.useState<Date>();
+  const [absences, setAbsenses] = React.useState<number[]>([]);
+
+  const { data: allAbsences } = api.absence.getAllAbsences.useQuery();
+  
+  React.useEffect(() => {
+    if (allAbsences) {
+      const foundAbsences: number[] = [];
+      allAbsences.forEach((absence) => {
+        if (new Date(absence.absenceDate).toDateString() == selectedDate?.toDateString()) {
+          foundAbsences.push(absence.participantId);
+        }
+      });
+      setAbsenses(foundAbsences);
+    }
+    
+  }, [selectedDate, allAbsences]);
+
+  return (
+    <div style={{padding: "32px"}}>
+      <div 
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center"
+        }}
+      >
+        <h3 
+          style={{
+            fontSize: "20px",
+            paddingRight: "12px"
+          }}
+        >Absentees for </h3>
+        <Popover>
+          <PopoverTrigger 
+            asChild 
+            style={selectedDate ? selectedDateStyle : noSelectedDateStyle}
+          >
+            <Button
+              variant={"outline"}
+              className={cn(
+                "justify-start text-left font-normal",
+                !selectedDate && "text-muted-foreground"
+              )}
+              style={{minWidth: "170px"}}
+            >
+              {selectedDate ? format(selectedDate, "PPP") : <span>Select Date</span>}
+              {DropdownIcon(!!selectedDate)}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div style={{paddingTop: "32px"}}>
+        {
+          selectedDate
+            ? AbsencesTable(absences)
+            : <div style={{fontSize: "16px", padding: "16px"}}>Pick a Date to View Absentees</div>
+        }
+      </div>
+    </div>
+  )
+}
+
+function AbsencesTable(absences: number[]) {
+  return (
+    absences.length > 0 
+      ? <table style={{width: "100%"}}>
+          <thead style={{backgroundColor: "#EFF2FB"}}>
+            <tr style={{}}>
+              <th style={{fontWeight: "normal", textAlign: "left", padding: "12px 16px 12px 32px", width: "200px"}}>S. No.</th>
+              <th style={{fontWeight: "normal", textAlign: "left", padding: "12px 32px 12px 16px"}}>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {absences.map((participantId) => {
+              return (
+                <tr key={participantId}>
+                  <td style={{padding: "12px 16px 12px 32px", width: "200px"}}>{participantId}</td>
+                  <td style={{padding: "12px 32px 12px 16px"}}>{`User ${participantId}`}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      : <div style={{fontSize: "16px", padding: "16px"}}>No Absentees</div>
+  );
+}
+
+function DropdownIcon(isSelected: boolean) {
+  return (
+    <svg 
+      className="ml-auto h-4 w-4" 
+      width="16" 
+      height="9" 
+      viewBox="0 0 16 9" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+        d="M15 0.999999L8 8L1 1" 
+        stroke={isSelected ? "white" : "black"} 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ apps/nextjs/src/lib/utils.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/consistent-type-specifier-style */
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
