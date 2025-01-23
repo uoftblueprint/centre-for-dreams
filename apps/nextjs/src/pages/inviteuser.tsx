dd5e99ae6d7e1f3f45733beb7c48d49c1763427e
elsiezhu <elsie.zhu2003@gmail.com>
2025-01-23 18:18:39 -0500

    fix formatting errors

--- apps/nextjs/src/pages/inviteuser.tsx
+++ apps/nextjs/src/pages/inviteuser.tsx
@@ -100,32 +100,96 @@ export default function InviteUser() {
           <a href="/">Create New</a>
         </button>
       </div>
-      <div style={{
+      <div
+        style={{
           display: "flex",
           flexDirection: "column",
           width: "-webkit-fill-available",
           justifyContent: "center",
-        }}>
-        <div style={{fontSize: "32px", textAlign: "center", paddingBottom: "36px"}}>Invite new user</div>
-        <form onSubmit={(e) => e.preventDefault()} style={{display: "flex", flexDirection: "column", justifyContent: "center", paddingBottom: "16px"}}>
-          <div style={{display: "flex", flexDirection: "column", width: "50%", justifyContent: "center", margin: "0 auto", paddingBottom: "24px"}}>
-            <label htmlFor="email" style={{color: "#000000", fontSize: "16px"}}>Email</label>
-            <input type="text" id="email" style={{padding: "8px", backgroundColor: "#EFF2FB", borderRadius: "10px"}}/>
-          </div>
-          <div style={{display: "flex", flexDirection: "column", width: "50%", justifyContent: "center", margin: "0 auto", paddingBottom: "36px"}}>
-            <label htmlFor="password" style={{color: "#000000", fontSize: "16px"}}>Temporary Password</label>
-            <input type="password" id="password" style={{padding: "8px", backgroundColor: "#EFF2FB", borderRadius: "10px"}}/>
-          </div>
-          <button 
+        }}
+      >
+        <div
+          style={{
+            fontSize: "32px",
+            textAlign: "center",
+            paddingBottom: "36px",
+          }}
+        >
+          Invite new user
+        </div>
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            paddingBottom: "16px",
+          }}
+        >
+          <div
             style={{
-              width: "50%", 
+              display: "flex",
+              flexDirection: "column",
+              width: "50%",
+              justifyContent: "center",
+              margin: "0 auto",
+              paddingBottom: "24px",
+            }}
+          >
+            <label
+              htmlFor="email"
+              style={{ color: "#000000", fontSize: "16px" }}
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              style={{
+                padding: "8px",
+                backgroundColor: "#EFF2FB",
+                borderRadius: "10px",
+              }}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "50%",
+              justifyContent: "center",
+              margin: "0 auto",
+              paddingBottom: "36px",
+            }}
+          >
+            <label
+              htmlFor="password"
+              style={{ color: "#000000", fontSize: "16px" }}
+            >
+              Temporary Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              style={{
+                padding: "8px",
+                backgroundColor: "#EFF2FB",
+                borderRadius: "10px",
+              }}
+            />
+          </div>
+          <button
+            style={{
+              width: "50%",
               backgroundColor: "#2E4D90",
               borderRadius: "24px",
               padding: "8px",
               margin: "0 auto",
-              color: "#FFFFFF"
+              color: "#FFFFFF",
             }}
-          >Invite</button>
+          >
+            Invite
+          </button>
         </form>
       </div>
     </div>
