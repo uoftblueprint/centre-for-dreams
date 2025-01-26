b80b3c7b4d30ad7f591cf499b48e627874bf23cd
Dani3lx <1danielxu@gmail.com>
2025-01-26 16:45:06 -0500

    Revert activities.tsx

--- apps/nextjs/src/pages/activities.tsx
+++ apps/nextjs/src/pages/activities.tsx
@@ -2,17 +2,16 @@ import React from "react";
 
 import CreateActivity from "../components/activity/activityform";
 import ViewActivities from "../components/activity/viewactivites";
-import ActivityModal from "~/components/activity/activitymodal";
 
 const Activities = () => {
+  const [open, setOpen] = React.useState(true)
   return (
-    // <div className="h-full w-full p-4">
-    //   <div className="font-medium">Create Activity</div>
-    //   <CreateActivity />
-    //   <div className="font-medium">View Activites</div>
-    //   <ViewActivities />
-    // </div>
-    <ActivityModal />
+    <div className="h-full w-full p-4">
+      <div className="font-medium">Create Activity</div>
+      <CreateActivity />
+      <div className="font-medium">View Activites</div>
+      <ViewActivities />
+    </div>
   );
 };
 
