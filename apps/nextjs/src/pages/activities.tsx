6763fc9fa0948fafeb8046a5b4406d89f8b138b9
Dani3lx <1danielxu@gmail.com>
2025-01-26 16:47:17 -0500

    Clean up

--- apps/nextjs/src/pages/activities.tsx
+++ apps/nextjs/src/pages/activities.tsx
@@ -4,7 +4,6 @@ import CreateActivity from "../components/activity/activityform";
 import ViewActivities from "../components/activity/viewactivites";
 
 const Activities = () => {
-  const [open, setOpen] = React.useState(true)
   return (
     <div className="h-full w-full p-4">
       <div className="font-medium">Create Activity</div>
