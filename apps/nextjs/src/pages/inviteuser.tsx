4b7647a2b6626d2e128b2b06bba0ab2eb3e9b7b1
elsiezhu <elsie.zhu2003@gmail.com>
2025-02-02 20:47:34 -0500

    change css to tailwind and move navbar into separate component

--- apps/nextjs/src/pages/inviteuser.tsx
+++ apps/nextjs/src/pages/inviteuser.tsx
@@ -1,193 +1,64 @@
 import React from "react";
 import { SignInButton, useAuth, UserButton } from "@clerk/nextjs";
 
+import NavBar from "~/components/navbar";
+
 export default function InviteUser() {
   const { isSignedIn } = useAuth();
   return (
-    <div
-      className="flex"
-      style={{
-        display: "flex",
-        position: "absolute",
-        width: "100%",
-        top: "0px",
-        bottom: "0px",
-      }}
-    >
-      <div
-        className="m-10 flex"
-        style={{
-          flexDirection: "column",
-          background: "#EFF2FB",
-          margin: "0",
-          padding: "16px 40px",
-          maxWidth: "40%",
-          justifyContent: "space-between",
-          position: "sticky",
-          top: 0,
-        }}
-      >
+    <div className="absolute bottom-0 top-0 flex w-full">
+      <NavBar />
+      {/* <div className="sticky top-0 m-0 flex max-w-[40%] flex-col justify-between bg-[#EFF2FB] p-[16px_40px]">
         {isSignedIn ? "" : <SignInButton />}
         <UserButton afterSignOutUrl="/" showName />
         <nav style={{ display: "flex", flexDirection: "column" }}>
-          <button
-            style={{
-              borderWidth: "1px",
-              borderColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "8px",
-              backgroundColor: "#2E4D90",
-              color: "#FFFFFF",
-            }}
-          >
+          <button className="m-2 rounded-[24px] border border-[#2E4D90] bg-[#2E4D90] p-2 text-white">
             <a href="/posts/">Forum</a>
           </button>
-          <button
-            style={{
-              borderWidth: "1px",
-              borderColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "8px",
-            }}
-          >
+          <button className="m-2 rounded-[24px] border border-[#2E4D90] p-2">
             <a href="/announcements/">Announcements</a>
           </button>
-          <button
-            style={{
-              borderWidth: "1px",
-              borderColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "8px",
-            }}
-          >
+          <button className="m-2 rounded-[24px] border border-[#2E4D90] p-2">
             <a href="/absences/">Absentees</a>
           </button>
-          <button
-            style={{
-              borderWidth: "1px",
-              borderColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "8px",
-            }}
-          >
+          <button className="m-2 rounded-[24px] border border-[#2E4D90] p-2">
             <a href="/activities/">Calendar</a>
           </button>
-          <button
-            style={{
-              borderWidth: "1px",
-              borderColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "8px",
-            }}
-          >
+          <button className="m-2 rounded-[24px] border border-[#2E4D90] p-2">
             <a href="/inviteuser/">Invite New User</a>
           </button>
         </nav>
-        <button
-          style={{
-            borderWidth: "1px",
-            borderColor: "#2E4D90",
-            borderRadius: "24px",
-            padding: "8px",
-            margin: "8px",
-          }}
-        >
+        <button className="m-2 rounded-[24px] border border-[#2E4D90] p-2">
           <a href="/">Create New</a>
         </button>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "-webkit-fill-available",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            fontSize: "32px",
-            textAlign: "center",
-            paddingBottom: "36px",
-          }}
-        >
-          Invite new user
-        </div>
+      </div> */}
+      <div className="flex w-full flex-col justify-center">
+        <div className="pb-9 text-center text-2xl">Invite new user</div>
         <form
           onSubmit={(e) => e.preventDefault()}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            paddingBottom: "16px",
-          }}
+          className="flex flex-col justify-center pb-4"
         >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "50%",
-              justifyContent: "center",
-              margin: "0 auto",
-              paddingBottom: "24px",
-            }}
-          >
-            <label
-              htmlFor="email"
-              style={{ color: "#000000", fontSize: "16px" }}
-            >
+          <div className="mx-auto flex w-1/2 flex-col justify-center pb-6">
+            <label htmlFor="email" className="text-base text-black">
               Email
             </label>
             <input
               type="text"
               id="email"
-              style={{
-                padding: "8px",
-                backgroundColor: "#EFF2FB",
-                borderRadius: "10px",
-              }}
+              className="rounded-[10px] bg-[#EFF2FB] p-2"
             />
           </div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "50%",
-              justifyContent: "center",
-              margin: "0 auto",
-              paddingBottom: "36px",
-            }}
-          >
-            <label
-              htmlFor="password"
-              style={{ color: "#000000", fontSize: "16px" }}
-            >
+          <div className="mx-auto flex w-1/2 flex-col justify-center pb-8">
+            <label htmlFor="password" className="text-base text-black">
               Temporary Password
             </label>
             <input
               type="password"
               id="password"
-              style={{
-                padding: "8px",
-                backgroundColor: "#EFF2FB",
-                borderRadius: "10px",
-              }}
+              className="rounded-[10px] bg-[#EFF2FB] p-2"
             />
           </div>
-          <button
-            style={{
-              width: "50%",
-              backgroundColor: "#2E4D90",
-              borderRadius: "24px",
-              padding: "8px",
-              margin: "0 auto",
-              color: "#FFFFFF",
-            }}
-          >
+          <button className="mx-auto w-1/2 rounded-2xl bg-[#2E4D90] p-2 text-white">
             Invite
           </button>
         </form>
