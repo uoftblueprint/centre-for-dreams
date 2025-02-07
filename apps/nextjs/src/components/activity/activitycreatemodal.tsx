65a0b0a066b2d1be7c1a7e48f178e02583d2f5be
Dani3lx <1danielxu@gmail.com>
2025-02-06 23:00:12 -0500

    Implemented create and update pop up

--- apps/nextjs/src/components/activity/activitycreatemodal.tsx
+++ apps/nextjs/src/components/activity/activitycreatemodal.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+"use client";
+
+import React, { useState } from "react";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
@@ -8,21 +10,36 @@ import { api } from "~/utils/api";
 import type { RouterInputs } from "~/utils/api";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 export type CreateAnnouncement = RouterInputs["activity"]["createActivity"];
 
-const ActivityCreateModal = ({
-  open,
-  setOpen,
-}: {
-  open: boolean;
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
-  const [date, setDate] = useState<Date | null>();
-  const [startTime, setStartTime] = useState<Date | null>();
+interface CreateActivity {
+  name: string;
+  location: string;
+  day: Date;
+  startTime: Date;
+  durationMinutes: number;
+  leader: string;
+}
 
-  const { register, handleSubmit, reset } = useForm<CreateAnnouncement>();
+export default function ActivityCreateModal() {
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState<Date | null>();
+  const [startTime, setStartTime] = useState<Date | null>(new Date());
+
+  const { register, handleSubmit, reset } = useForm<CreateActivity>();
 
   const [isSuccess, setIsSuccess] = useState(false);
 
@@ -43,164 +60,143 @@ const ActivityCreateModal = ({
     }
   };
 
-  const onSubmit = (data: CreateAnnouncement) => {
+  const onSubmit = (data: CreateActivity) => {
     data.day = date ?? new Date();
     data.startTime = startTime ?? new Date();
     data.durationMinutes = parseInt(data.durationMinutes.toString(), 10);
-    createActivity({
-      name: data.name,
-      location: data.location,
-      day: data.day,
-      startTime: data.startTime,
-      durationMinutes: data.durationMinutes,
-      leader: data.leader,
-    });
+    createActivity(data);
   };
 
-  useEffect(() => {
-    setDate(new Date());
-  }, []);
-
   return (
-    <div
-      className={`fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black/80 ${
-        open ? "" : "invisible"
-      }`}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <form className="m-12 flex w-full min-w-[320px] flex-col gap-3 rounded-lg border-b-2 bg-white p-6 font-semibold max-sm:text-sm sm:w-2/3 sm:p-12 lg:w-1/2 xl:w-2/5">
-        <div className="flex flex-col gap-2">
-          <label htmlFor="name">Title</label>
-          <input
-            required
-            {...register("name", { required: true })}
-            name="name"
-            onChange={handleInputChange}
-            id="name"
-            type="text"
-            className="rounded-md bg-slate-200 p-3 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
-          />
-        </div>
-        <div className="flex flex-col gap-3 lg:flex-row">
-          <div className="flex flex-col gap-2 lg:w-2/5">
-            <label htmlFor="day">Date</label>
-            <Popover>
-              <PopoverTrigger
-                asChild
-                className="h-full w-full rounded-md border-none bg-slate-200 p-3 font-normal focus:bg-gray-100 focus:outline-none"
-                id="day"
-              >
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "justify-start text-left font-normal",
-                    !date && "text-muted-foreground",
-                  )}
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">Create Activity</Button>
+      </DialogTrigger>
+
+      <DialogContent className="p-8 font-semibold">
+        <DialogHeader>
+          <DialogTitle>Create Activity</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="name">Title</Label>
+            <Input
+              required
+              {...register("name", { required: true })}
+              name="name"
+              onChange={handleInputChange}
+              id="name"
+              type="text"
+              className="h-full w-full bg-slate-200 p-3 font-normal hover:outline-none focus:bg-gray-100 focus:outline-none"
+            />
+          </div>
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <div className="flex flex-col gap-2 lg:w-2/5">
+              <Label htmlFor="day">Date</Label>
+              <Popover>
+                <PopoverTrigger
+                  asChild
+                  className="h-full w-full rounded-md border-none bg-slate-200 p-3 font-normal focus:bg-gray-100 focus:outline-none"
+                  id="day"
                 >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar
-                  mode="single"
-                  selected={date ?? new Date()}
-                  onSelect={setDate}
-                  initialFocus
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "justify-start text-left font-normal",
+                      !date && "text-muted-foreground",
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={date ?? new Date()}
+                    onSelect={setDate}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
+
+            <div className="flex gap-2 lg:w-3/5">
+              <div className="flex w-full flex-col gap-2">
+                <Label htmlFor="startTime">Time</Label>
+                <input
+                  onChange={(e) => {
+                    const [hours, minutes] = e.target.value
+                      .split(":")
+                      .map(Number);
+                    const time = new Date();
+                    time.setUTCHours(hours ?? 0, minutes, 0, 0);
+                    setStartTime(time);
+                  }}
+                  value={
+                    startTime
+                      ? new Date(
+                          startTime.toLocaleString("en-US", {
+                            timeZone: "America/New_York",
+                          }),
+                        )
+                          .toISOString()
+                          .substring(11, 16)
+                      : ""
+                  }
+                  id="startTime"
+                  required
+                  name="startTime"
+                  type="time"
+                  className="w-full cursor-pointer rounded-md bg-slate-200 p-3 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
                 />
-              </PopoverContent>
-            </Popover>
-          </div>
-
-          <div className="flex w-full gap-2">
-            <div className="flex w-full flex-col gap-2">
-              <label htmlFor="startTime">Time</label>
-              <input
-                onChange={(e) => {
-                  const [hours, minutes] = e.target.value
-                    .split(":")
-                    .map(Number);
-                  const time = new Date();
-                  time.setUTCHours(hours ?? 0, minutes, 0, 0);
-                  setStartTime(time);
-                }}
-                value={
-                  startTime
-                    ? new Date(
-                        startTime.toLocaleString("en-US", {
-                          timeZone: "America/New_York",
-                        }),
-                      )
-                        .toISOString()
-                        .substring(11, 16)
-                    : ""
-                }
-                id="startTime"
-                required
-                name="startTime"
-                type="time"
-                className="w-full cursor-pointer rounded-md bg-slate-200 p-3 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
-              />
-            </div>
-            <div className="flex w-full flex-col gap-2">
-              <label htmlFor="durationMinutes">Duration (Minutes)</label>
-              <input
-                {...register("durationMinutes", { required: true })}
-                id="durationMinutes"
-                name="durationMinutes"
-                required
-                min={15}
-                type="number"
-                className="w-full cursor-pointer rounded-md bg-slate-200 p-3    text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
-              />
+              </div>
+              <div className="flex w-full flex-col gap-2">
+                <Label htmlFor="durationMinutes">Duration</Label>
+                <Input
+                  {...register("durationMinutes", { required: true })}
+                  id="durationMinutes"
+                  name="durationMinutes"
+                  required
+                  min={15}
+                  type="number"
+                  className="h-full w-full cursor-pointer rounded-md bg-slate-200 p-3 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="location">Location</label>
-          <input
-            {...register("location", { required: true })}
-            id="location"
-            onChange={handleInputChange}
-            required
-            name="location"
-            type="text"
-            className="w- rounded-md bg-slate-200 p-3 text-sm    font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Input
+              {...register("location", { required: true })}
+              id="location"
+              onChange={handleInputChange}
+              required
+              name="location"
+              type="text"
+              className="w- rounded-md bg-slate-200 p-3 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
+            />
+          </div>
 
-        <div className="flex flex-col gap-2">
-          <label htmlFor="leader">Leader</label>
-          <input
-            {...register("leader", { required: true })}
-            id="leader"
-            onChange={handleInputChange}
-            required
-            name="leader"
-            type="text"
-            className="w- rounded-md bg-slate-200 p-3 text-sm    font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
-          />
-        </div>
-
-        <div className="flex gap-3 self-end text-center text-xs sm:text-sm">
-          <button
-            className="w-fit rounded-full bg-gray-200 px-5 py-3 font-normal text-black sm:px-10"
-            type="button"
-            onClick={() => setOpen(false)}
-          >
-            Cancel
-          </button>
-          <button
-            className="w-fit rounded-full bg-black px-3 py-3 font-normal text-white sm:px-5"
-            type="submit"
-          >
-            Create Activity
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="leader">Leader</Label>
+            <Input
+              {...register("leader", { required: true })}
+              id="leader"
+              onChange={handleInputChange}
+              required
+              name="leader"
+              type="text"
+              className="w- rounded-md bg-slate-200 text-sm font-normal hover:bg-slate-200/70 focus:bg-gray-100 focus:outline-none"
+            />
+          </div>
+          <DialogFooter>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   );
-};
-
-export default ActivityCreateModal;
+}
