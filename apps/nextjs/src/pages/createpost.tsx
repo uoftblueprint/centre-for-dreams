f9eae339cee2fe3c9a7d04d734af2e9660f45b7e
Adventurer-E <erickudou@gmail.com>
2025-02-09 18:39:51 -0500

    added shadcn ui dependencies

--- apps/nextjs/src/pages/createpost.tsx
+++ apps/nextjs/src/pages/createpost.tsx
@@ -2,6 +2,25 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 
+import { Button } from "../components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select"
+
 import { api } from "~/utils/api";
 import styles from "../styles/createpost.module.css";
 
