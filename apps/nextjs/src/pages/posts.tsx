19881231ce67d29ac8a5b039f7e33cd04323feae
Sarina Li <sarinajin.li@gmail.com>
2025-03-08 03:17:42 -0500

    refactor sidebar + other minor fixes

--- apps/nextjs/src/pages/posts.tsx
+++ apps/nextjs/src/pages/posts.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { SignInButton, useAuth, UserButton } from "@clerk/nextjs";
+import React, { useState } from "react";git
 import type { PostType } from "@prisma/client";
 
+import NavBar from "~/components/navbar";
 import { api } from "~/utils/api";
 import Post from "../components/post";
 import ToggleButton from "../components/ToggleButton";
@@ -38,34 +38,9 @@ function Posts() {
     setMyPostToggle(false);
   };
 
-  const { isSignedIn } = useAuth();
-
   return (
-    <div className="absolute bottom-0 top-0 flex w-full">
-      <div className="sticky top-0 flex max-w-[40%] flex-col justify-between bg-[#EFF2FB] p-4">
-        {isSignedIn ? "" : <SignInButton />}
-        <UserButton afterSignOutUrl="/" showName />
-        <nav className="flex flex-col">
-          <button className="m-2 rounded-3xl border border-[#2E4D90] bg-[#2E4D90] p-2 text-white">
-            <a href="/posts/">Forum</a>
-          </button>
-          <button className="m-2 rounded-3xl border border-[#2E4D90] p-2">
-            <a href="/announcements/">Announcements</a>
-          </button>
-          <button className="m-2 rounded-3xl border border-[#2E4D90] p-2">
-            <a href="/absences/">Absentees</a>
-          </button>
-          <button className="m-2 rounded-3xl border border-[#2E4D90] p-2">
-            <a href="/activities/">Calendar</a>
-          </button>
-          <button className="m-2 rounded-3xl border border-[#2E4D90] p-2">
-            <a href="/activities/">Invite New User</a>
-          </button>
-        </nav>
-        <button className="m-2 rounded-3xl border border-[#2E4D90] p-2">
-          <a href="/activities/">Create New</a>
-        </button>
-      </div>
+    <div className="relative flex">
+      <NavBar></NavBar>
       <div className="flex w-full flex-col items-center pt-6">
         <ToggleButton
           word="Posts"
