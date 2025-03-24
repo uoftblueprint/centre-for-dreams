c2ddf0da970e906652ab158c95a61cd5e8868f7c
Sarina Li <sarinajin.li@gmail.com>
2025-03-23 22:20:30 -0400

    body tag

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import { useAuth } from "@clerk/nextjs";
 
 import NavBar from "~/components/navbar";
 import SignedInLanding from "~/components/signed_in_landing";
 import SignedOutLanding from "~/components/signed_out_landing";
+import { NoSsr } from "../components/no-ssr";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   const { isSignedIn } = useAuth();
 
   if (isSignedIn) {
     return (
-      <div className="absolute bottom-0 top-0 flex w-full">
-        <NavBar />
-        <div className="flex w-full flex-col justify-center">
-          <SignedInLanding />
+      <NoSsr>
+        <div className="absolute bottom-0 top-0 flex w-full">
+          <NavBar />
+          <div className="flex w-full flex-col justify-center">
+            <SignedInLanding />
+          </div>
         </div>
-      </div>
+      </NoSsr>
     );
   }
 
