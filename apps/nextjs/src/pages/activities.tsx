e2fb5f64bd9abdf90ad0f07cf89093a6fdb2143f
Sarina Li <sarinajin.li@gmail.com>
2025-03-23 22:34:53 -0400

    remove navbar

--- apps/nextjs/src/pages/activities.tsx
+++ apps/nextjs/src/pages/activities.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import ActivityCreateModal from "~/components/activity/activitycreatemodal";
 import ActivityUpdateModal from "~/components/activity/activityupdatemodal";
-import NavBar from "~/components/navbar";
+// import NavBar from "~/components/navbar";
 import CreateActivity from "../components/activity/activityform";
 import ViewActivities from "../components/activity/viewactivites";
 
 const Activities = () => {
   return (
     <div className="relative flex">
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="flex-col items-center pl-6 pt-6">
         <ActivityUpdateModal id={1} />
         <ActivityCreateModal />
