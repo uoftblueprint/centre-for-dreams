16a38808cad0422580579a8e02583d6d00c76b2f
Sarina Li <sarinajin.li@gmail.com>
2025-03-25 23:44:45 -0400

    table component

--- apps/nextjs/src/pages/absences.tsx
+++ apps/nextjs/src/pages/absences.tsx
@@ -1,54 +1,230 @@
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
+// import React, { useState } from "react";
 
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
+
+// import { api } from "~/utils/api";
+// import Absence from "../components/absence";
+
+// export default function Absences() {
+//   const { data: allAbsences } = api.absence.getAllAbsences.useQuery();
+//   const [startDate, setStartDate] = useState(new Date());
+
+//   const areDatesEqual = (date1: Date, date2: Date) => {
+//     return (
+//       date1.getFullYear() === date2.getFullYear() &&
+//       date1.getMonth() === date2.getMonth() &&
+//       date1.getDate() === date2.getDate()
+//     );
+//   };
+
+//   const filteredAbsences = allAbsences
+//     ?.filter(
+//       (absence) => !startDate || areDatesEqual(absence.absenceDate, startDate),
+//     )
+//     .map((data, index) => (
+//       <Absence
+//         key={index}
+//         userId={data.participantId}
+//         absenceDate={data.absenceDate}
+//         activityName={data.activity.name}
+//       />
+//     ));
+
+//   return (
+//     <div className="relative flex">
+//       <NavBar />
+//       <div className="flex w-full flex-col p-4">
+//         <h1 className="text-2xl font-extrabold">Absences</h1>
+//         <div className="my-4">
+//           <span>Pick a date: </span>
+//           <DatePicker
+//             selected={startDate}
+//             onChange={(date: Date) => setStartDate(date)}
+//           />
+//         </div>
+//         {filteredAbsences}
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from "react";
 
 import NavBar from "~/components/navbar";
-import { api } from "~/utils/api";
-import Absence from "../components/absence";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../components/ui/table";
 
-const DatePicker = dynamic(() => import("react-datepicker"), {
-  ssr: false,
-});
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
 export default function Absences() {
-  const { data: allAbsences } = api.absence.getAllAbsences.useQuery();
-  const [startDate, setStartDate] = useState(new Date());
-
-  const areDatesEqual = (date1: Date, date2: Date) => {
-    return (
-      date1.getFullYear() === date2.getFullYear() &&
-      date1.getMonth() === date2.getMonth() &&
-      date1.getDate() === date2.getDate()
-    );
-  };
-
-  const filteredAbsences = allAbsences
-    ?.filter(
-      (absence) => !startDate || areDatesEqual(absence.absenceDate, startDate),
-    )
-    .map((data, index) => (
-      <Absence
-        key={index}
-        userId={data.participantId}
-        absenceDate={data.absenceDate}
-        activityName={data.activity.name}
-      />
-    ));
-
   return (
     <div className="relative flex">
       <NavBar />
-      <div className="flex w-full flex-col p-4">
-        <h1 className="text-2xl font-extrabold">Absences</h1>
-        <div className="my-4">
-          <span>Pick a date: </span>
-          <DatePicker
-            selected={startDate}
-            onChange={(date: Date) => setStartDate(date)}
-          />
+      <div className="flex h-screen w-full justify-center px-16 py-16">
+        <div className="w-full">
+          <h1>Tuitle</h1>
+      <Table 
+            className=""
+            containerClassname="h-fit max-h-[600px] overflow-y-auto relative"
+          >
+          <TableCaption>List of absentees.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((invoice) => (
+              <TableRow key={invoice.invoice}>
+                <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                <TableCell>{invoice.paymentStatus}</TableCell>
+                <TableCell>{invoice.paymentMethod}</TableCell>
+                <TableCell className="text-right">
+                  {invoice.totalAmount}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
         </div>
-        {filteredAbsences}
       </div>
     </div>
   );
